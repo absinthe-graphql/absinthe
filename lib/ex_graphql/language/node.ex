85a488d5e88b8cee1370f3d0163db5ec64d46d8f
Bruce Williams <brwcodes@gmail.com>
2015-10-19 19:10:14 -0700

    Fill out validation rule, prep for type info work

--- lib/ex_graphql/language/node.ex
+++ lib/ex_graphql/language/node.ex
@@ -1,6 +1,12 @@
 defprotocol ExGraphQL.Language.Node do
 
+  @fallback_to_any true
+
   @spec children(ExGraphQL.Language.Node.t) :: [ExGraphQL.Language.Node.t]
   def children(node)
 
+end
+
+defimpl ExGraphQL.Language.Node, for: Any do
+  def children(node), do: []
 end
--- lib/ex_graphql/validation/rules/arguments_of_correct_type.ex
+++ lib/ex_graphql/validation/rules/arguments_of_correct_type.ex
@@ -1,8 +1,20 @@
 defmodule ExGraphQL.Validation.Rules.ArgumentsOfCorrectType do
 
+  alias ExGraphQL.Validation.Context
+  alias ExGraphQL.Utility
+
   @spec check(ExGraphQL.Validation.Context.t, ExGraphQL.Language.Node.t) :: [binary]
   def check(context, %{__struct__: mod}) do
-    [mod]
+    arg_def = context |> Context.argument
+    if arg_def && Utility.valid_literal_value?(arg_def.type, node.value) do
+      [error(node, arg_def)]
+    else
+      []
+    end
+  end
+
+  defp error(node, arg_def) do
+    "Argument \"#{node.name.value}\" expected type \"#{arg_def.type}\" but got: #{inspect(node.value)}."
   end
 
 end
