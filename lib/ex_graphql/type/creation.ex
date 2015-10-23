eb2c75dd00c52fa24e5987e4ce5427a4a9693e1e
Bruce Williams <brwcodes@gmail.com>
2015-10-22 22:51:02 -0700

    Fix type setup hook

--- lib/ex_graphql/type/creation.ex
+++ lib/ex_graphql/type/creation.ex
@@ -4,16 +4,16 @@ defmodule ExGraphQL.Type.Creation do
     defexception type: nil, message: "could not create"
   end
 
-  defmacro __using__(mod, options) do
+  defmacro __using__(options) do
     quote do
       def create(values) do
-        Kernel.struct(unquote(mod), values)
-        |> unquote(mod).setup
+        Kernel.struct(__MODULE__, values)
+        |> __MODULE__.setup
       end
       def create!(values) do
         case create(values) do
           {:ok, result} -> result
-          {:error, error} = raise ExGraphQL.Type.Creation.Error, type: unquote(mod), message: error
+          {:error, error} -> raise unquote(__MODULE__).Error, type: __MODULE__, message: error
         end
       end
     end
--- lib/ex_graphql/type/schema.ex
+++ lib/ex_graphql/type/schema.ex
@@ -5,7 +5,7 @@ defmodule ExGraphQL.Type.Schema do
   def setup(struct) do
     # TODO: Should also introspect struct, see graphql-js' `__Struct`
     types_to_map = [struct.query, struct.mutation]
-    case types_to_map |> &ExGraphQL.Type.TypeMap.build(%{}) do
+    case types_to_map |> ExGraphQL.Type.TypeMap.build do
       {:ok, type_map} -> {:ok, %{struct | type_map: type_map}}
       {:error, _} = err -> err
     end
