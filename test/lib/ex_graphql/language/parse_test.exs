7f5f54a8fc3b31b9805e280c5ae8d86fe404d27d
Bruce Williams <brwcodes@gmail.com>
2015-12-10 20:58:08 -0800

    Passing mutation alias test

--- test/lib/ex_graphql/language/parse_test.exs
+++ test/lib/ex_graphql/language/parse_test.exs
@@ -5,4 +5,28 @@ defmodule ExGraphQL.Language.ParseTest do
     assert {:ok, %{__struct__: ExGraphQL.Language.Document}} = ExGraphQL.parse("{ hello }")
   end
 
+  @mutation_with_alias """
+  mutation ProvisionTopicsAndQueues {
+    queue1: createQueue(name: "new-queue1") {
+      url
+    }
+    queue2: createQueue(name: "new-queue2") {
+      url
+    }
+    topic1: createTopic(name: "new-topic1") {
+      arn
+    }
+    topic2: createTopic(name: "new-topic2") {
+      arn
+    }
+    topic3: createTopic(name: "new-topic3") {
+      arn
+    }
+  }
+  """
+
+  test "can parse a mutation with an alias" do
+    assert {:ok, doc} = ExGraphQL.parse(@mutation_with_alias)
+  end
+
 end
