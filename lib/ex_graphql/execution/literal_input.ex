f458b4b6fb02d631a8385c0f0aee8706cd1f1448
Bruce Williams <brwcodes@gmail.com>
2015-12-11 17:42:07 -0800

    Fix coercion of input variables

--- lib/ex_graphql/execution/literal_input.ex
+++ lib/ex_graphql/execution/literal_input.ex
@@ -1,19 +1,21 @@
 defmodule ExGraphQL.Execution.LiteralInput do
 
+  alias ExGraphQL.Type
+
   def from_arguments(ast_arguments, schema_arguments, variables) do
     schema_arguments
     |> Enum.reduce(%{}, fn ({name, definition}, acc) ->
-      ast_arg = ast_arguments |> Enum.find(&(String.to_atom(&1.name) == String.to_atom(name)))
+      ast_arg = ast_arguments |> Enum.find(&(String.to_atom(&1.name) == name))
       if ast_arg do
         ast_value = if ast_arg.value do
-          Type.coerce(definition.type, ast_arg.value)
+          Type.coerce(definition.type, ast_arg.value.value)
         else
           nil
         end
         variable_value = variables[ast_arg.name]
         default_value = definition.default_value
         acc
-        |> Map.put(ast_value || variable_value || default_value)
+        |> Map.put(name, ast_value || variable_value || default_value)
       else
         acc
       end
