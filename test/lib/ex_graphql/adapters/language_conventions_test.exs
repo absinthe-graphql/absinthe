861537fdf969c70dd446ad2068f1495ea54dbc4d
Bruce Williams <brwcodes@gmail.com>
2015-12-16 22:56:19 -0800

    Failing tests for basic LanguageConventions adapter functionality

--- test/lib/ex_graphql/adapters/language_conventions_test.exs
+++ test/lib/ex_graphql/adapters/language_conventions_test.exs
@@ -1,6 +1,8 @@
-defmodule ExGraphQL.Adapters.LanguageConventions do
+defmodule ExGraphQL.Adapters.LanguageConventionsTest do
   use ExSpec, async: true
 
+  use ExGraphQL.Type
+  alias ExGraphQL.Type
   alias ExGraphQL.Adapters.LanguageConventions
 
   it "converts external camelcase field names to underscore" do
@@ -15,6 +17,120 @@ defmodule ExGraphQL.Adapters.LanguageConventions do
   end
   it "converts internal underscored variable names to camelcase external variable names" do
     assert "fooBar" = LanguageConventions.to_external_name("foo_bar", :variable)
+  end
+
+  defp field_trip_type do
+    %Type.ObjectType{
+      name: "FieldTrip",
+      description: "A field_trip",
+      fields: fields(
+        id: [
+          type: %Type.NonNull{of_type: Type.Scalar.string},
+          description: "The ID of the field trip"
+        ],
+        name: [
+          type: Type.Scalar.string,
+          description: "The name of field trip is located"
+        ],
+        location_name: [
+          type: Type.Scalar.string,
+          description: "The place the field trip is located"
+        ],
+        other_field_trip: [
+          type: field_trip_type,
+          resolve: fn (_, _exe, %{target: %{id: id}}) ->
+            case id do
+              "museum" -> {:ok, field_trips |> Map.get("opera_house")}
+              "opera_house" -> {:ok, field_trips |> Map.get("museum")}
+            end
+          end
+        ]
+      )
+    }
+  end
+
+  defp field_trips do
+    %{
+      "museum" => %{id: "museum", name: "Museum", location_name: "Portland"},
+      "opera_house" => %{id: "opera_house", name: "Opera House", location_name: "Sydney"}
+     }
+  end
+
+  defp simple_schema do
+    %Type.Schema{
+      query: %Type.ObjectType{
+        name: "RootQuery",
+        fields: fields(
+          bad_resolution: [
+            type: field_trip_type,
+            resolve: fn(_args, _exe, _res) ->
+              :not_expected
+            end
+          ],
+          field_trip_by_context: [
+            type: field_trip_type,
+            resolve: fn
+              (_args, %{context: %{field_trip: id}}, _res) -> {:ok, field_trips |> Map.get(id)}
+              (_args, _exe, _res) -> {:error, "No :id context provided"}
+            end
+          ],
+          field_trip: [
+            type: field_trip_type,
+            args: args(
+              id: [
+                description: "id of the field trip",
+                type: %Type.NonNull{of_type: Type.Scalar.string}
+              ]
+            ),
+            resolve: fn
+              (%{"id" => id}, _exe, _res) ->
+                {:ok, field_trips |> Map.get(id)}
+            end
+          ]
+        )
+      }
+    }
+  end
+
+  it "can do a simple query" do
+    query = """
+    query GimmeMuseum {
+      fieldTrip(id: "museum") {
+        name
+        locationName
+      }
+    }
+    """
+    assert {:ok, %{data: %{"fieldTrip" => %{"name" => "Museum", "locationName" => "Portland"}}, errors: []}} = run(query)
+  end
+
+  it "can do a simple query with an alias" do
+    query = """
+      query GimmeMuseumWithAlias {
+        thePlace: fieldTrip(id: "museum") {
+          name
+          locationName
+        }
+      }
+    """
+    assert {:ok, %{data: %{"thePlace" => %{"name" => "Museum", "locationName" => "Portland"}}, errors: []}} = run(query)
+  end
+
+
+  it "can identify a bad field" do
+    query = """
+    {
+      fieldTrip(id: "museum") {
+        name
+        badField
+      }
+    }
+    """
+    assert {:ok, %{data: %{"fieldTrip" => %{"name" => "Museum"}}, errors: [%{message: "Field `badField': Not present in schema", locations: [%{line: 4, column: 0}]}]}} = run(query)
+  end
+
+  defp run(query_document) do
+    ExGraphQL.run(simple_schema, query_document, validate: false, adapter: LanguageConventions)
   end
 
 end
