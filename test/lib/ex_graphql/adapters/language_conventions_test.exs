4274164da6ce261544c73be8f9f649c8ec946c98
Bruce Williams <brwcodes@gmail.com>
2015-12-16 20:19:19 -0800

    Start of LanguageConventions adapter

---
+++ test/lib/ex_graphql/adapters/language_conventions_test.exs
@@ -0,0 +1,20 @@
+defmodule ExGraphQL.Adapters.LanguageConventions do
+  use ExSpec, async: true
+
+  alias ExGraphQL.Adapters.LanguageConventions
+
+  it "converts external camelcase field names to underscore" do
+    assert "foo_bar" = LanguageConventions.to_internal_name("fooBar", :field)
+  end
+  it "converts external camelcase variable names to underscore" do
+    assert "foo_bar" = LanguageConventions.to_internal_name("fooBar", :variable)
+  end
+
+  it "converts internal underscored field names to camelcase external field names" do
+    assert "fooBar" = LanguageConventions.to_external_name("foo_bar", :field)
+  end
+  it "converts internal underscored variable names to camelcase external variable names" do
+    assert "fooBar" = LanguageConventions.to_external_name("foo_bar", :variable)
+  end
+
+end
