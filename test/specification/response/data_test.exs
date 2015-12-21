d6e838c0b52dba3f405928ec5b72e6c7b192eca6
Bruce Williams <brwcodes@gmail.com>
2015-12-21 15:15:58 -0800

    Start of specification tests

---
+++ test/specification/response/data_test.exs
@@ -0,0 +1,22 @@
+defmodule Specification.Response.DataTest do
+  use ExSpec, async: true
+
+  @graphql_spec "#sec-Data"
+
+  describe "if the operation was a query" do
+    it "will be an object of the schema's query root type"
+  end
+
+  describe "if the operation was a mutation" do
+    it "will be an object of the schema's mutation root type"
+  end
+
+  describe "if an error occurs before execution begins" do
+    it "should not be present"
+  end
+
+  describe "if an error occurs that prevented a valid response" do
+    test "data should be null"
+  end
+
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,3 @@
-Code.require_file("test/star_wars/schema.exs")
-Code.require_file("test/star_wars/data.exs")
-
 Code.require_file("test/lib/ex_graphql/type/fixtures.exs")
-
 
 ExUnit.start()
