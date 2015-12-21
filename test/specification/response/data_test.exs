99a4605aac7fe3b70ca521c0cf927624d01d474e
Bruce Williams <brwcodes@gmail.com>
2015-12-21 15:56:07 -0800

    Sketch out "Response" tests

--- test/specification/response/data_test.exs
+++ test/specification/response/data_test.exs
@@ -3,20 +3,24 @@ defmodule Specification.Response.DataTest do
 
   @graphql_spec "#sec-Data"
 
-  describe "if the operation was a query" do
-    it "will be an object of the schema's query root type"
-  end
+  describe "the data entry in the response" do
 
-  describe "if the operation was a mutation" do
-    it "will be an object of the schema's mutation root type"
-  end
+    describe "if the operation was a query" do
+      it "will be an object of the schema's query root type"
+    end
 
-  describe "if an error occurs before execution begins" do
-    it "should not be present"
-  end
+    describe "if the operation was a mutation" do
+      it "will be an object of the schema's mutation root type"
+    end
 
-  describe "if an error occurs that prevented a valid response" do
-    test "data should be null"
+    describe "if an error occurs before execution begins" do
+      it "should not be present"
+    end
+
+    describe "if an error occurs that prevented a valid response" do
+      it "should be null"
+    end
+
   end
 
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,4 @@
 Code.require_file("test/lib/ex_graphql/type/fixtures.exs")
 
+ExUnit.configure(exclude: [pending: true])
 ExUnit.start()
