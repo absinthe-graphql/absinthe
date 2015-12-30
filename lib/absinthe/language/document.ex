e83ac038fc27317e97d0ef06bf72974d40dc06fd
Bruce Williams <brwcodes@gmail.com>
2015-12-29 16:18:00 -0800

    Support getting an operation definition by name

--- lib/absinthe/language/document.ex
+++ lib/absinthe/language/document.ex
@@ -1,11 +1,23 @@
 defmodule Absinthe.Language.Document do
-  # The parsed AST representation of a query document"
+  @moduledoc "The parsed AST representation of a query document"
 
-  @moduledoc false
+  alias __MODULE__
 
   @typedoc false
   @type t :: %{definitions: [Absinthe.Traversal.Node.t], loc: Absinthe.Language.loc_t}
   defstruct definitions: [], loc: %{start_line: nil}
+
+  @doc "Extract a named operation definition from a document"
+  @spec get_operation(t, binary) :: nil | Absinthe.Language.OperationDefinition.t
+  def get_operation(%{definitions: definitions}, name) do
+    definitions
+    |> Enum.find(nil, fn
+      %{name: ^name} ->
+        true
+      _ ->
+        false
+    end)
+  end
 
   @doc false
   @spec fragments_by_name(Absinthe.Language.Document.t) :: %{binary => Absinthe.Language.FragmentDefinition.t}
---
+++ test/lib/absinthe/language/document_test.exs
@@ -0,0 +1,46 @@
+defmodule Absinthe.Languguage.DocumentTest do
+  use ExSpec, async: true
+
+  alias Absinthe.Language.Document
+  alias Absinthe.Language.OperationDefinition
+
+  @input """
+  query MyQuery1 {
+    thing(id: "1") {
+      name
+    }
+  }
+  query MyQuery2 {
+    thing(id: "1") {
+      name
+    }
+  }
+  mutation MyMutation {
+    thing(id: "1") {
+      name
+    }
+  }
+
+  """
+
+  describe "get_operation/2" do
+
+    describe "given an existing operation name" do
+
+      it "returns the operation definition" do
+        doc = Absinthe.parse!(@input)
+        result = Document.get_operation(doc, "MyQuery2")
+        assert %OperationDefinition{name: "MyQuery2", operation: :query} = result
+      end
+
+    end
+
+    describe "given a non-existing operation name" do
+      doc = Absinthe.parse!(@input)
+      result = Document.get_operation(doc, "DoesNotExist")
+      assert nil == result
+    end
+
+  end
+
+end
