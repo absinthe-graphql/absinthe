cd652a6b5a9d025d091697e1153e6b1687c42192
Bruce Williams <brwcodes@gmail.com>
2016-01-11 01:19:05 -0800

    Docs

--- README.md
+++ README.md
@@ -278,6 +278,100 @@ Now `:iso_z` can be used in your schema and variables can use
 You can introspect your schema using `__schema`, `__type`, and `__typename`,
 as [described in the specification](https://facebook.github.io/graphql/#sec-Introspection).
 
+### Examples
+
+Seeing the names of the types in the schema:
+
+```
+"""
+{
+  __schema {
+    types {
+      name
+    }
+  }
+}
+"""
+|> Absinthe.run(MyApp.Schema)
+{:ok,
+  %{data: %{
+    "__schema" => %{
+      "types" => [
+        %{"name" => "Boolean"},
+        %{"name" => "Float"},
+        %{"name" => "ID"},
+        %{"name" => "Int"},
+        %{"name" => "String"},
+        ...
+      ]
+    }
+  }}
+}
+```
+
+Getting the name of the queried type:
+
+```elixir
+"""
+{
+  profile {
+    name
+    __typename
+  }
+}
+"""
+|> Absinthe.run(MyApp.Schema)
+{:ok,
+  %{data: %{
+    "profile" => %{
+      "name" => "Joe",
+      "__typename" => "Person"
+    }
+  }}
+}
+```
+
+Getting the name of the fields for a named type:
+
+```elixir
+"""
+{
+  __type(name: "Person") {
+    fields {
+      name
+      type {
+        kind
+        name
+      }
+    }
+  }
+}
+"""
+|> Absinthe.run(MyApp.Schema)
+{:ok,
+  %{data: %{
+    "__type" => %{
+      "fields" => [
+        %{
+          "name" => "name",
+          "type" => %{"kind" => "SCALAR", "name" => "String"}
+        },
+        %{
+          "name" => "age",
+          "type" => %{"kind" => "SCALAR", "name" => "Int"}
+        },
+      ]
+    }
+  }}
+}
+```
+
+(Note that you may have to nest several depths of `type`/`ofType`, as
+type information includes any wrapping layers of [List](https://facebook.github.io/graphql/#sec-List)
+and/or [NonNull](https://facebook.github.io/graphql/#sec-Non-null).)
+
+### Limitations
+
 See [the PR](https://github.com/CargoSense/absinthe/pull/26) for details on
 limitations (notably enum values, union types, and directives cannot currently
 be introspected).
--- lib/absinthe/introspection.ex
+++ lib/absinthe/introspection.ex
@@ -6,9 +6,97 @@ defmodule Absinthe.Introspection do
   You can introspect your schema using `__schema`, `__type`, and `__typename`,
   as [described in the specification](https://facebook.github.io/graphql/#sec-Introspection).
 
-  See [the PR](https://github.com/CargoSense/absinthe/pull/26) for details on
-  limitations (notably enum values, union types, and directives cannot currently
-  be introspected).
+  ## Examples
+
+  Seeing the names of the types in the schema:
+
+  ```
+  \"""
+  {
+    __schema {
+      types {
+        name
+      }
+    }
+  }
+  \"""
+  |> Absinthe.run(MyApp.Schema)
+  {:ok,
+    %{data: %{
+      "__schema" => %{
+        "types" => [
+          %{"name" => "Boolean"},
+          %{"name" => "Float"},
+          %{"name" => "ID"},
+          %{"name" => "Int"},
+          %{"name" => "String"},
+          ...
+        ]
+      }
+    }}
+  }
+  ```
+
+  Getting the name of the queried type:
+
+  ```
+  \"""
+  {
+    profile {
+      name
+      __typename
+    }
+  }
+  \"""
+  |> Absinthe.run(MyApp.Schema)
+  {:ok,
+    %{data: %{
+      "profile" => %{
+        "name" => "Joe",
+        "__typename" => "Person"
+      }
+    }}
+  }
+  ```
+
+  Getting the name of the fields for a named type:
+
+  ```
+  \"""
+  {
+    __type(name: "Person") {
+      fields {
+        name
+        type {
+          kind
+          name
+        }
+      }
+    }
+  }
+  \"""
+  |> Absinthe.run(MyApp.Schema)
+  {:ok,
+    %{data: %{
+      "__type" => %{
+        "fields" => [
+          %{
+            "name" => "name",
+            "type" => %{"kind" => "SCALAR", "name" => "String"}
+          },
+          %{
+            "name" => "age",
+            "type" => %{"kind" => "SCALAR", "name" => "Int"}
+          },
+        ]
+      }
+    }}
+  }
+  ```
+
+  (Note that you may have to nest several depths of `type`/`ofType`, as
+  type information includes any wrapping layers of [List](https://facebook.github.io/graphql/#sec-List)
+  and/or [NonNull](https://facebook.github.io/graphql/#sec-Non-null).)
   """
 
 end
