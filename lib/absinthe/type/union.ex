81de3f8a7992b295279c506347ec61f305fc076f
Bruce Williams <brwcodes@gmail.com>
2016-01-27 14:13:32 -0800

    Union documentation

--- lib/absinthe/type/union.ex
+++ lib/absinthe/type/union.ex
@@ -1,17 +1,50 @@
 defmodule Absinthe.Type.Union do
 
-  # TODO: Unions are not yet fully supported
-  @moduledoc false
+  @moduledoc """
+  A unions is an abstract type made up of multiple possible concrete types.
+
+  No common fields are declared in a union. Compare to `Absinthe.Type.Interface`.
+
+  Because it's necessary for the union to determine the concrete type of a
+  resolved object, you must either:
+
+  * Provide a `:resolve_type` function on the union
+  * Provide a `:is_type_of` function on each possible concrete type
+
+  ```
+  @absinthe :type
+  def search_result do
+    %Type.Union{
+      description: "A search result that can be a person or business",
+      types: [:person, :business],
+      resolve_type: fn
+        %{age: _}, _ -> {:ok, :person}
+        %{employee_count: _}, _ -> {:ok, :business}
+        _ -> :error
+      end
+    }
+  end
+  ```
+
+  * `:name` - The name of the union type. Should be a TitleCased `binary`. Set automatically when using `@absinthe :type` from `Absinthe.Type.Definitions`.
+  * `:description` - A nice description for introspection.
+  * `:types` - The list of possible types.
+  * `:resolve_type` - A function used to determine the concrete type of a resolved object. See also `Absinthe.Type.Object`'s `:is_type_of`.
+
+  The `:resolve_type` function will be passed two arguments; the object whose type needs to be identified, and the `Absinthe.Execution` struct providing the full execution context.
+
+  The `:reference` key is for internal use.
+  """
 
   use Absinthe.Introspection.Kind
 
   alias Absinthe.Type
 
-  @typedoc false
   @type t :: %{name: binary,
                description: binary,
                types: [Absinthe.Type.t],
-               resolve_type: ((any, Absinthe.Execution.t) -> atom | nil), reference: Type.Reference.t}
+               resolve_type: ((any, Absinthe.Execution.t) -> atom | nil),
+               reference: Type.Reference.t}
 
   defstruct name: nil, description: nil, resolve_type: nil, types: [], reference: nil
 
@@ -21,6 +54,7 @@ defmodule Absinthe.Type.Union do
     |> Enum.member?(type)
   end
 
+  @doc false
   @spec resolve_type(t, any, Execution.t) :: Type.t | nil
   def resolve_type(%{resolve_type: nil, types: types}, obj, %{schema: schema}) do
     Enum.find(types, fn
