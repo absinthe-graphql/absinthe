bce2c93b92eb9fc61801f8d1725c008a46a27001
Bruce Williams <brwcodes@gmail.com>
2016-02-03 13:28:00 -0800

    Try using Macro.expand

--- lib/absinthe/schema/definition.ex
+++ lib/absinthe/schema/definition.ex
@@ -13,7 +13,8 @@ defmodule Absinthe.Schema.Definition do
   end
 
   defmacro object(identifier, attrs) do
-    Absinthe.Type.Object.build(identifier, attrs)
+    blueprint = Macro.expand(attrs, __CALLER__)
+    Absinthe.Type.Object.build(identifier, blueprint)
   end
 
   defmacro interface(identifier, attrs) do
@@ -22,5 +23,51 @@ defmodule Absinthe.Schema.Definition do
   defmacro input_object(identifier, attrs) do
   end
 
+
+  @doc """
+  Deprecate a field or argument with an optional reason
+
+  ## Examples
+
+  Wrap around an argument or a field definition
+  (of a `Absinthe.Type.InputObject`) to deprecate it:
+
+  ```
+  args(
+    name: deprecate([type: :string, description: "The person's name"])
+    # ...
+  )
+  ```
+
+  You can also provide a reason:
+
+  ```
+  args(
+    age: deprecate([type: :integer, description: "The person's age"],
+                   reason: "None of your business!")
+    # ...
+  )
+  ```
+
+  Some usage information for deprecations:
+
+  * They make non-null types nullable.
+  * Currently use of a deprecated argument/field causes an error to be added to the `:errors` entry of a result.
+  """
+  @spec deprecate(Keyword.t) :: Keyword.t
+  defmacro deprecate(node, options \\ []) do
+    IO.inspect(node, options: options)
+    quote do: node
+  end
+
+  @doc "Add a non-null constraint to a type"
+  defmacro non_null(type) do
+    quote do: %Absinthe.Type.NonNull{of_type: unquote(type)}
+  end
+
+  @doc "Declare a list of a type"
+  defmacro list_of(type) do
+    quote do: %Absinthe.Type.List{of_type: unquote(type)}
+  end
 
 end
