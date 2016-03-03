32aaa16148ded12a9b4d3610e820ed0203e8b4bd
Ben Wilson <benwilson512@gmail.com>
2016-03-03 10:32:11 -0500

    Fix bugs where a list of scalars could not be resolved. Fixes a bug where lists returned in reverse order

--- test/lib/absinthe/execution/list_test.exs
+++ test/lib/absinthe/execution/list_test.exs
@@ -1,10 +1,25 @@
 defmodule Absinthe.Execution.ListTest.Schema do
   use Absinthe.Schema
 
+  object :item do
+    field :categories, list_of(:string)
+  end
+
   query do
+    field :numbers, list_of(:integer), resolve: fn _, _ -> {:ok, [1,2,3]} end
     field :categories, list_of(:string) do
       resolve fn _, _ ->
         {:ok, ["foo", "bar", "baz"]}
+      end
+    end
+
+    field :items, list_of(:item) do
+      resolve fn _, _ ->
+        items = [
+          %{categories: ["foo", "bar"]},
+          %{categories: ["baz", "buz"]},
+        ]
+        {:ok, items}
       end
     end
   end
@@ -19,9 +34,34 @@ defmodule Absinthe.Execution.ListTest do
   }
   """
 
-  it "should not blow up" do
-    Absinthe.run(@query, __MODULE__.Schema)
-    |> IO.inspect
+  it "should resolve list of strings" do
+    assert {:ok, %{data: %{"categories" => ["foo", "bar", "baz"]}}} ==
+      Absinthe.run(@query, __MODULE__.Schema)
   end
+
+  @query """
+  {
+    numbers
+  }
+  """
+
+  it "should resolve list of numbers" do
+    assert {:ok, %{data: %{"numbers" => [1,2,3]}}} ==
+      Absinthe.run(@query, __MODULE__.Schema)
+  end
+
+  @query """
+  {
+    items {
+      categories
+    }
+  }
+  """
+
+  it "should resolve list of objects with a list of scalars inside" do
+    assert {:ok, %{data: %{"items" => [%{"categories" => ["foo", "bar"]}, %{"categories" => ["baz", "buz"]}]}}} ==
+      Absinthe.run(@query, __MODULE__.Schema)
+  end
+
 
 end
