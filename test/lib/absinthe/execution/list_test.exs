87edd5cc16924ff04f93dd96116232a24fa4b749
Maxence <maxence.decrosse@gmail.com>
2016-03-04 11:20:41 +0100

    Allows for List of Lists

--- test/lib/absinthe/execution/list_test.exs
+++ test/lib/absinthe/execution/list_test.exs
@@ -5,6 +5,10 @@ defmodule Absinthe.Execution.ListTest.Schema do
     field :categories, list_of(:string)
   end
 
+  object :book do
+    field :name, :string
+  end
+
   query do
     field :numbers, list_of(:integer), resolve: fn _, _ -> {:ok, [1,2,3]} end
     field :categories, list_of(:string) do
@@ -19,6 +23,34 @@ defmodule Absinthe.Execution.ListTest.Schema do
           %{categories: ["foo", "bar"]},
           %{categories: ["baz", "buz"]},
         ]
+        {:ok, items}
+      end
+    end
+
+    field :list_of_list_of_numbers, list_of(list_of(:integer)) do
+      resolve fn _, _ -> {:ok, [[1, 2, 3], [4, 5, 6]]} end
+    end
+
+    field :list_of_list_of_books, list_of(list_of(:book)) do
+      resolve fn _, _ ->
+        books = [[
+          %{name: "foo"},
+          %{name: "bar"},
+        ], [
+          %{name: "baz"},
+        ]]
+        {:ok, books}
+      end
+    end
+
+    field :list_of_list_of_items, list_of(list_of(:item)) do
+      resolve fn _, _ ->
+        items = [[
+          %{categories: ["foo", "bar"]},
+          %{categories: ["baz", "buz"]},
+        ], [
+          %{categories: ["blip", "blop"]},
+        ]]
         {:ok, items}
       end
     end
@@ -63,5 +95,50 @@ defmodule Absinthe.Execution.ListTest do
       Absinthe.run(@query, __MODULE__.Schema)
   end
 
+  @query """
+  {
+    listOfListOfNumbers
+  }
+  """
+  it "should resolve list of list of numbers" do
+    assert {:ok, %{data: %{"listOfListOfNumbers" => [[1,2,3],[4,5,6]]}}} ==
+      Absinthe.run(@query, __MODULE__.Schema)
+  end
+
+  @query """
+  {
+    listOfListOfBooks {
+      name
+    }
+  }
+  """
+  it "should resolve list of list of books" do
+    books = [[
+      %{"name" => "foo"},
+      %{"name" => "bar"},
+    ], [
+      %{"name" => "baz"},
+    ]]
+    assert {:ok, %{data: %{"listOfListOfBooks" => books}}} ==
+      Absinthe.run(@query, __MODULE__.Schema)
+  end
+
+  @query """
+  {
+    listOfListOfItems {
+      categories
+    }
+  }
+  """
+  it "should resolve list of list of items" do
+    items = [[
+      %{"categories" => ["foo", "bar"]},
+      %{"categories" => ["baz", "buz"]},
+    ], [
+      %{"categories" => ["blip", "blop"]},
+    ]]
+    assert {:ok, %{data: %{"listOfListOfItems" => items}}} ==
+      Absinthe.run(@query, __MODULE__.Schema)
+  end
 
 end
