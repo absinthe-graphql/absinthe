1dd7af20e17b19784f148ec5545c53c638c93658
Bruce Williams <brwcodes@gmail.com>
2016-07-03 15:39:30 -0700

    Pipeline configuration

--- lib/absinthe/pipeline.ex
+++ lib/absinthe/pipeline.ex
@@ -2,9 +2,13 @@ defmodule Absinthe.Pipeline do
 
   alias Absinthe.Phase
 
-  @type t :: [Phase.t]
+  @type input_t :: any
+  @type output_t :: any
+  @type phase_config_t :: Phase.t | {Phase.t, any}
 
-  @spec run(any, t) :: {:ok, any} | {:error, Phase.Error.t}
+  @type t :: [phase_config_t]
+
+  @spec run(input_t, t) :: {:ok, output_t} | {:error, Phase.Error.t}
   def run(input, pipeline) do
     case do_run(input, pipeline) do
       {:error, _} = err ->
@@ -14,19 +18,33 @@ defmodule Absinthe.Pipeline do
     end
   end
 
+  @bad_return "Phase did not return an {:ok, any} | {:error, Absinthe.Phase.Error.t} | {:error, String.t}"
+
+  @spec do_run(input_t, t) :: {:ok, output_t} | {:error, Phase.Error.t}
   defp do_run(input, pipeline) do
-    Enum.reduce_while(pipeline, input, fn phase, item ->
-      case phase.run(item) do
+    Enum.reduce_while(pipeline, input, fn config, item ->
+      {phase, args} = phase_invocation(config, item)
+      case apply(phase, :run, args) do
         {:ok, result} ->
           {:cont, result}
         {:error, %Phase.Error{}} = err ->
           {:halt, err}
         {:error, message} ->
-          IO.inspect(message)
           err = {:error, %Phase.Error{message: message, phase: phase}}
+          {:halt, err}
+        _ ->
+          err = {:error, %Phase.Error{message: @bad_return, phase: phase}}
           {:halt, err}
       end
     end)
+  end
+
+  @spec phase_invocation(phase_config_t, input_t) :: {Phase.t, list}
+  defp phase_invocation({phase, args}, item) do
+    {phase, [item | [args]]}
+  end
+  defp phase_invocation(phase, item) do
+    {phase, [item]}
   end
 
   @spec for_operation :: t
--- test/lib/absinthe/pipeline_test.exs
+++ test/lib/absinthe/pipeline_test.exs
@@ -1,7 +1,7 @@
 defmodule Absinthe.PipelineTest do
   use Absinthe.Case, async: true
 
-  alias Absinthe.{Blueprint, Pipeline}
+  alias Absinthe.{Blueprint, Pipeline, Phase}
 
   describe '.run an operation' do
 
@@ -27,6 +27,49 @@ defmodule Absinthe.PipelineTest do
       assert {:ok, %Blueprint{}} = Pipeline.run(@query, Pipeline.for_schema)
     end
 
+  end
+
+  defmodule Phase1 do
+    def run(input) do
+      {:ok, String.reverse(input)}
+    end
+  end
+
+  defmodule Phase2 do
+    def run(input, options) do
+      result = (1..options[:times])
+      |> Enum.map(fn _ -> input end)
+      |> Enum.join(".")
+      {:ok, result}
+    end
+  end
+
+  defmodule Phase3 do
+    def run(input, %{reverse: true}) do
+      {:ok, String.reverse(input)}
+    end
+    def run(input, %{reverse: false}) do
+      {:ok, input}
+    end
+  end
+
+  describe ".run with options" do
+    it "should work" do
+      assert {:ok, "oof.oof.oof"} == Pipeline.run("foo", [Phase1, {Phase2, times: 3}, {Phase3, %{reverse: false}}])
+      assert {:ok, "foo.foo.foo"} == Pipeline.run("foo", [Phase1, {Phase2, times: 3}, {Phase3, %{reverse: true}}])
+    end
+  end
+
+  defmodule BadPhase do
+    def run(input) do
+      input
+    end
+  end
+
+  describe ".run with a bad phase result" do
+    it "should return a nice error object" do
+      assert {:error, %Phase.Error{phase: BadPhase, message: "Phase did not return an {:ok, any} | {:error, Absinthe.Phase.Error.t} | {:error, String.t}"}} ==  Pipeline.run("foo", [BadPhase])
+    end
   end
 
 end
