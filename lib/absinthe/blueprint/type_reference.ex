aa6fea8e56b6d2f487319730e21929b22b9f84ca
Bruce Williams <brwcodes@gmail.com>
2016-07-05 00:31:28 -0700

    Blueprint.TypeReference.unwrap/1

--- lib/absinthe/blueprint/type_reference.ex
+++ lib/absinthe/blueprint/type_reference.ex
@@ -1,4 +1,4 @@
-defmodule Blueprint.TypeReference do
+defmodule Absinthe.Blueprint.TypeReference do
 
   alias __MODULE__
 
@@ -6,5 +6,18 @@ defmodule Blueprint.TypeReference do
       TypeReference.List.t
     | TypeReference.Name.t
     | TypeReference.NonNull.t
+
+  @wrappers [TypeReference.List, TypeReference.NonNull]
+
+  @doc """
+  Unwrap a type reference from surrounding NonNull/List type information.
+  """
+  @spec unwrap(t) :: t
+  def unwrap(%TypeReference.Name{} = value) do
+    value
+  end
+  def unwrap(%struct{of_type: inner}) when struct in @wrappers do
+    unwrap(inner)
+  end
 
 end
