c49c4f77f089488f8f1563e40604c21a56b97084
Bruce Williams <brwcodes@gmail.com>
2016-07-26 15:33:13 -0700

    Build out assertions for validation test harness

--- test/support/harness/validation.ex
+++ test/support/harness/validation.ex
@@ -1,30 +1,45 @@
 defmodule Support.Harness.Validation do
   import ExUnit.Assertions
 
-  alias Absinthe.{Phase, Pipeline}
+  alias Absinthe.{Blueprint, Schema, Phase, Pipeline, Language}
 
-  def expect_valid(schema, rules, document, provided_values) do
-    # TODO
+  @type error_matcher_t :: ({Blueprint.node_t, Phase.Error.t} -> boolean)
+
+  @spec assert_valid(Schema.t, [Phase.t], Language.Source.t, map) :: no_return
+  def assert_valid(schema, rules, document, provided_values) do
+    {:ok, result} = run(schema, rules, document, provided_values)
+    assert Enum.empty?(nodes_with_errors(result))
   end
 
-  def expect_invalid(schema, rules, document, provided_values, error_matcher) do
-    # TODO
+  @spec assert_invalid(Schema.t, [Phase.t], Language.Source.t, map, error_matcher_t) :: no_return
+  def assert_invalid(schema, rules, document, provided_values, error_matcher) do
+    {:ok, result} = run(schema, rules, document, provided_values)
+    pairs = nodes_with_errors(result)
+    |> Enum.flat_map(fn
+      %{errors: errors} = node ->
+        Enum.map(errors, &{node, &1})
+    end)
+    assert Enum.any?(pairs, error_matcher)
   end
 
-  def expect_passes_rule(rule, document, provided_values) do
-    expect_valid(Support.Harness.Validation.Schema, [rule], document, provided_values)
+  @spec assert_passes_rule(Phase.t, Language.Source.t, map) :: no_return
+  def assert_passes_rule(rule, document, provided_values) do
+    assert_valid(Support.Harness.Validation.Schema, [rule], document, provided_values)
   end
 
-  def expect_fails_rule(rule, document, provided_values, error_matcher) do
-    expect_invalid(Support.Harness.Validation.Schema, [rule], document, provided_values, error_matcher)
+  @spec assert_fails_rule(Phase.t, Language.Source.t, map, error_matcher_t) :: no_return
+  def assert_fails_rule(rule, document, provided_values, error_matcher) do
+    assert_invalid(Support.Harness.Validation.Schema, [rule], document, provided_values, error_matcher)
   end
 
-  def expect_passes_rule_with_schema(schema, rule, document, provided_values) do
-    expect_valid(schema, [rule], document, provided_values)
+  @spec assert_passes_rule_with_schema(Schema.t, Phase.t, Language.Source.t, map) :: no_return
+  def assert_passes_rule_with_schema(schema, rule, document, provided_values) do
+    assert_valid(schema, [rule], document, provided_values)
   end
 
-  def expect_fails_rule_with_schema(schema, rule, document, provided_values, error_matcher) do
-    expect_invalid(schema, [rule], document, provided_values, error_matcher)
+  @spec assert_fails_rule_with_schema(Schema.t, Phase.t, Language.Source.t, map, error_matcher_t) :: no_return
+  def assert_fails_rule_with_schema(schema, rule, document, provided_values, error_matcher) do
+    assert_invalid(schema, [rule], document, provided_values, error_matcher)
   end
 
   defp run(schema, rules, document, provided_values) do
@@ -45,18 +60,18 @@ defmodule Support.Harness.Validation do
   end
 
   # Build a map of node => errors
-  defp harvest_errors(input) do
-    {result, errors} = Blueprint.prewalk(input, %{}, &do_harvest_errors/2)
+  defp nodes_with_errors(input) do
+    {result, errors} = Blueprint.prewalk(input, [], &do_nodes_with_errors/2)
     errors
   end
 
-  defp do_harvest_errors(%{errors: []} = node, acc) do
+  defp do_nodes_with_errors(%{errors: []} = node, acc) do
     {node, acc}
   end
-  defp do_harvest_errors(%{errors: errors} = node, acc) do
-    {node, Map.put(acc, node, errors)}
+  defp do_nodes_with_errors(%{errors: errors} = node, acc) do
+    {node, [node | acc]}
   end
-  defp do_harvest_errors(node, acc) do
+  defp do_nodes_with_errors(node, acc) do
     {node, acc}
   end
 
