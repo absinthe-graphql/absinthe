7207ab0c51612e53b7b056e1ec3ccdf7d757a449
Shane Howley <howleysv@gmail.com>
2016-11-13 18:44:52 +0000

    Always use the field resolvers of the concrete object type (#199)
    * Always use the field resolvers of the concrete object type
    
    * Fix bad merge

--- test/lib/absinthe/union_fragment_test.exs
+++ test/lib/absinthe/union_fragment_test.exs
@@ -5,13 +5,17 @@ defmodule Absinthe.UnionFragmentTest do
     use Absinthe.Schema
 
     object :user do
-      field :name, :string
+      field :name, :string do
+        resolve fn user, _, _ -> {:ok, user.username} end
+      end
       field :todos, list_of(:todo)
       interface :named
     end
 
     object :todo do
-      field :name, :string
+      field :name, :string do
+        resolve fn todo, _, _ -> {:ok, todo.title} end
+      end
       field :completed, :boolean
       interface :named
       interface :completable
@@ -34,6 +38,7 @@ defmodule Absinthe.UnionFragmentTest do
 
     object :viewer do
       field :objects, list_of(:object)
+      field :me, :user
       field :named_thing, :named
     end
 
@@ -42,11 +47,12 @@ defmodule Absinthe.UnionFragmentTest do
         resolve fn _, _ ->
           {:ok, %{
             objects: [
-              %{type: :user, name: "foo", completed: true},
-              %{type: :todo, name: "do stuff", completed: false},
-              %{type: :user, name: "bar"},
+              %{type: :user, username: "foo", completed: true},
+              %{type: :todo, title: "do stuff", completed: false},
+              %{type: :user, username: "bar"},
             ],
-            named_thing: %{type: :todo, name: "do stuff", completed: false}
+            me: %{type: :user, username: "baz", todos: [], name: "should not be exposed"},
+            named_thing: %{type: :todo, title: "do stuff", completed: false}
           }}
         end
       end
@@ -76,6 +82,24 @@ defmodule Absinthe.UnionFragmentTest do
       %{"__typename" => "Todo", "completed" => false},
       %{"__typename" => "User", "name" => "bar"},
     ]}}
+    assert {:ok, %{data: expected}} == Absinthe.run(doc, Schema)
+  end
+
+  test "it queries an interface with the concrete type's field resolvers" do
+    doc = """
+    {
+      viewer {
+        me {
+          ... on Named {
+            __typename
+            name
+          }
+        }
+      }
+    }
+
+    """
+    expected = %{"viewer" => %{"me" => %{"__typename" => "User", "name" => "baz"}}}
     assert {:ok, %{data: expected}} == Absinthe.run(doc, Schema)
   end
 
@@ -121,4 +145,5 @@ defmodule Absinthe.UnionFragmentTest do
     }}
     assert {:ok, %{data: expected}} == Absinthe.run(doc, Schema)
   end
+
 end
