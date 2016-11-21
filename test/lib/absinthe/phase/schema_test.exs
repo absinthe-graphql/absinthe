de9e41671e0f120f1c379c1a53c636f5a1821c55
Bruce Williams <brwcodes@gmail.com>
2016-11-21 14:48:30 -0800

    Handle schema application more gracefully when provided a bad list (#213)

---
+++ test/lib/absinthe/phase/schema_test.exs
@@ -0,0 +1,41 @@
+defmodule Absinthe.Phase.SchemaTest do
+  use Absinthe.Case, async: true
+
+  describe "when given [Int] for Int schema node" do
+
+    defmodule IntegerInputSchema do
+      use Absinthe.Schema
+
+      query do
+
+        field :test, :string do
+          arg :integer, :integer
+          resolve fn
+            _, _, _ ->
+              {:ok, "ayup"}
+          end
+        end
+
+      end
+
+    end
+
+
+    @query """
+    { test(integer: [1]) }
+    """
+
+    it "doesn't raise an exception" do
+      assert {:ok, _} = run(@query)
+    end
+
+  end
+
+  def run(query) do
+    with {:ok, value} <- Absinthe.Phase.Parse.run(query),
+         {:ok, value} <- Absinthe.Phase.Blueprint.run(value) do
+      Absinthe.Phase.Schema.run(value, schema: IntegerInputSchema)
+    end
+  end
+
+end
