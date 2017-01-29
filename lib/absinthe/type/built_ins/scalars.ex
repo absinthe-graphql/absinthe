455b8f7399d39fca360e52c999a8c726be9b6f81
Mickael Lucchini <hello@mickael.lu>
2017-01-29 08:53:40 -0500

    removed datetime types from built_ins

--- lib/absinthe/type/built_ins/scalars.ex
+++ lib/absinthe/type/built_ins/scalars.ex
@@ -59,48 +59,6 @@ defmodule Absinthe.Type.BuiltIns.Scalars do
     parse parse_with([Absinthe.Blueprint.Input.Boolean], &parse_boolean/1)
   end
 
-  scalar :utc_datetime, name: "DateTime" do
-    description """
-    The `UTC DateTime` scalar type represents a date and time in the UTC
-    timezone. The DateTime appears in a JSON response as an ISO8601 formatted
-    string, including UTC timezone ("Z").
-    """
-
-    serialize &DateTime.to_iso8601/1
-    parse parse_with([Absinthe.Blueprint.Input.DateTime], &parse_utc_datetime/1)
-  end
-
-  scalar :datetime, name: "NaiveDateTime" do
-    description """
-    The `DateTime` scalar type represents a naive date and time without
-    timezone. The DateTime appears in a JSON response as an ISO8601 formatted
-    string.
-    """
-
-    serialize &NaiveDateTime.to_iso8601/1
-    parse parse_with([Absinthe.Blueprint.Input.NaiveDateTime], &parse_datetime/1)
-  end
-
-  scalar :date do
-    description """
-    The `Date` scalar type represents a date. The Date appears in a JSON
-    response as an ISO8601 formatted string.
-    """
-
-    serialize &Date.to_iso8601/1
-    parse parse_with([Absinthe.Blueprint.Input.Date], &parse_date/1)
-  end
-
-  scalar :time do
-    description """
-    The `Time` scalar type represents a time. The Time appears in a JSON
-    response as an ISO8601 formatted string.
-    """
-
-    serialize &Time.to_iso8601/1
-    parse parse_with([Absinthe.Blueprint.Input.Time], &parse_time/1)
-  end
-
   # Integers are only safe when between -(2^53 - 1) and 2^53 - 1 due to being
   # encoded in JavaScript and represented in JSON as double-precision floating
   # point numbers, as specified by IEEE 754.
@@ -152,51 +110,6 @@ defmodule Absinthe.Type.BuiltIns.Scalars do
   defp parse_boolean(_) do
     :error
   end
-
-  @spec parse_utc_datetime(any) :: {:ok, DateTime.t} | :error
-  defp parse_utc_datetime(value) when is_binary(value) do
-    case DateTime.from_iso8601(value) do
-      {:ok, utc_datetime, _offset} -> {:ok, utc_datetime}
-      _error -> :error
-    end
-  end
-  defp parse_utc_datetime(_) do
-    :error
-  end
-
-  @spec parse_datetime(any) :: {:ok, NaiveDateTime.t} | :error
-  defp parse_datetime(value) when is_binary(value) do
-    case NaiveDateTime.from_iso8601(value) do
-      {:ok, datetime} -> {:ok, datetime}
-      _error -> :error
-    end
-  end
-  defp parse_datetime(_) do
-    :error
-  end
-
-  @spec parse_date(any) :: {:ok, Date.t} | :error
-  defp parse_date(value) when is_binary(value) do
-    case Date.from_iso8601(value) do
-      {:ok, date} -> {:ok, date}
-      _error -> :error
-    end
-  end
-  defp parse_date(_) do
-    :error
-  end
-
-  @spec parse_time(any) :: {:ok, Time.t} | :error
-  defp parse_time(value) when is_binary(value) do
-    case Time.from_iso8601(value) do
-      {:ok, time} -> {:ok, time}
-      _error -> :error
-    end
-  end
-  defp parse_time(_) do
-    :error
-  end
-
 
   # Parse, supporting pulling values out of blueprint Input nodes
   defp parse_with(node_types, coercion) do
