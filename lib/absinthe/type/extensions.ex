d5bd6e12e59beea380e7b447c0ed5f03976d827b
Mickael Lucchini <hello@mickael.lu>
2017-01-29 09:02:53 -0500

    moved datetime types to new extensions module

---
+++ lib/absinthe/type/extensions.ex
@@ -0,0 +1,111 @@
+defmodule Absinthe.Type.Extensions do
+  use Absinthe.Schema.Notation
+
+  @moduledoc """
+  This module contains additional data types.
+
+  To use: `import_types Absinthe.Type.Extensions`.
+  """
+
+  scalar :datetime, name: "DateTime" do
+    description """
+    The `DateTime` scalar type represents a date and time in the UTC
+    timezone. The DateTime appears in a JSON response as an ISO8601 formatted
+    string, including UTC timezone ("Z").
+    """
+
+    serialize &DateTime.to_iso8601/1
+    parse parse_with([Absinthe.Blueprint.Input.DateTime], &parse_datetime/1)
+  end
+
+  scalar :naive_datetime, name: "NaiveDateTime" do
+    description """
+    The `Naive DateTime` scalar type represents a naive date and time without
+    timezone. The DateTime appears in a JSON response as an ISO8601 formatted
+    string.
+    """
+
+    serialize &NaiveDateTime.to_iso8601/1
+    parse parse_with([Absinthe.Blueprint.Input.NaiveDateTime], &parse_naive_datetime/1)
+  end
+
+  scalar :date do
+    description """
+    The `Date` scalar type represents a date. The Date appears in a JSON
+    response as an ISO8601 formatted string.
+    """
+
+    serialize &Date.to_iso8601/1
+    parse parse_with([Absinthe.Blueprint.Input.Date], &parse_date/1)
+  end
+
+  scalar :time do
+    description """
+    The `Time` scalar type represents a time. The Time appears in a JSON
+    response as an ISO8601 formatted string.
+    """
+
+    serialize &Time.to_iso8601/1
+    parse parse_with([Absinthe.Blueprint.Input.Time], &parse_time/1)
+  end
+
+  @spec parse_datetime(any) :: {:ok, DateTime.t} | :error
+  defp parse_datetime(value) when is_binary(value) do
+    case DateTime.from_iso8601(value) do
+      {:ok, datetime, _offset} -> {:ok, datetime}
+      _error -> :error
+    end
+  end
+  defp parse_datetime(_) do
+    :error
+  end
+
+  @spec parse_naive_datetime(any) :: {:ok, NaiveDateTime.t} | :error
+  defp parse_naive_datetime(value) when is_binary(value) do
+    case NaiveDateTime.from_iso8601(value) do
+      {:ok, naive_datetime} -> {:ok, naive_datetime}
+      _error -> :error
+    end
+  end
+  defp parse_naive_datetime(_) do
+    :error
+  end
+
+  @spec parse_date(any) :: {:ok, Date.t} | :error
+  defp parse_date(value) when is_binary(value) do
+    case Date.from_iso8601(value) do
+      {:ok, date} -> {:ok, date}
+      _error -> :error
+    end
+  end
+  defp parse_date(_) do
+    :error
+  end
+
+  @spec parse_time(any) :: {:ok, Time.t} | :error
+  defp parse_time(value) when is_binary(value) do
+    case Time.from_iso8601(value) do
+      {:ok, time} -> {:ok, time}
+      _error -> :error
+    end
+  end
+  defp parse_time(_) do
+    :error
+  end
+
+
+  # Parse, supporting pulling values out of blueprint Input nodes
+  defp parse_with(node_types, coercion) do
+    fn
+      %{__struct__: str, value: value} ->
+        if Enum.member?(node_types, str) do
+          coercion.(value)
+        else
+          :error
+        end
+      other ->
+        coercion.(other)
+    end
+  end
+
+end
---
+++ test/lib/absinthe/type/extensions_test.exs
@@ -0,0 +1,171 @@
+defmodule Absinthe.Type.ExtensionsTest do
+  use Absinthe.Case, async: true
+
+  alias Absinthe.Type
+
+  defmodule TestSchema do
+    use Absinthe.Schema
+    import_types Type.Extensions
+  end
+
+  @datetime %DateTime{
+    year: 2017, month: 1, day: 27,
+    hour: 20, minute: 31, second: 55,
+    time_zone: "Etc/UTC", zone_abbr: "UTC", utc_offset: 0, std_offset: 0,
+  }
+  @naive_datetime ~N[2017-01-27 20:31:55]
+  @date ~D[2017-01-27]
+  @time ~T[20:31:55]
+
+  defp serialize(type, value) do
+    TestSchema.__absinthe_type__(type)
+    |> Type.Scalar.serialize(value)
+  end
+
+  defp parse(type, value) do
+    TestSchema.__absinthe_type__(type)
+    |> Type.Scalar.parse(value)
+  end
+
+  describe ":datetime" do
+    it "serializes as an ISO8601 date and time string with UTC timezone marker" do
+      assert "2017-01-27T20:31:55Z" == serialize(:datetime, @datetime)
+    end
+
+    it "can be parsed from an ISO8601 date and time string including timezone" do
+      assert {:ok, @datetime} == parse(:datetime, "2017-01-27T20:31:55Z")
+      assert {:ok, @datetime} == parse(:datetime, "2017-01-27 20:31:55Z")
+    end
+
+    it "cannot be parsed without UTC timezone marker" do
+      assert :error == parse(:datetime, "2017-01-27T20:31:55")
+      assert :error == parse(:datetime, "2017-01-27 20:31:55")
+    end
+
+    it "cannot be parsed when date or time is missing" do
+      assert :error == parse(:datetime, "2017-01-27")
+      assert :error == parse(:datetime, "20:31:55")
+    end
+
+    it "cannot be parsed from a binary not formatted according to ISO8601" do
+      assert :error == parse(:datetime, "abc123")
+      assert :error == parse(:datetime, "01/25/2017 20:31:55")
+      assert :error == parse(:datetime, "2017-15-42T31:71:95Z")
+    end
+
+    it "cannot be parsed from a number" do
+      assert :error == parse(:datetime, 0)
+      assert :error == parse(:datetime, 0.0)
+    end
+
+    it "cannot be parsed from a boolean" do
+      assert :error == parse(:datetime, true)
+      assert :error == parse(:datetime, false)
+    end
+  end
+
+  describe ":naive_datetime" do
+    it "serializes as an ISO8601 date and time string" do
+      assert "2017-01-27T20:31:55" == serialize(:naive_datetime, @naive_datetime)
+    end
+
+    it "can be parsed from an ISO8601 date and time string" do
+      assert {:ok, @naive_datetime} == parse(:naive_datetime, "2017-01-27T20:31:55Z")
+      assert {:ok, @naive_datetime} == parse(:naive_datetime, "2017-01-27 20:31:55Z")
+      assert {:ok, @naive_datetime} == parse(:naive_datetime, "2017-01-27 20:31:55")
+    end
+
+    it "cannot be parsed when date or time is missing" do
+      assert :error == parse(:naive_datetime, "2017-01-27")
+      assert :error == parse(:naive_datetime, "20:31:55")
+    end
+
+    it "cannot be parsed from a binary not formatted according to ISO8601" do
+      assert :error == parse(:naive_datetime, "abc123")
+      assert :error == parse(:naive_datetime, "01/25/2017 20:31:55")
+      assert :error == parse(:naive_datetime, "2017-15-42T31:71:95")
+    end
+
+    it "cannot be parsed from a number" do
+      assert :error == parse(:naive_datetime, 0)
+      assert :error == parse(:naive_datetime, 0.0)
+    end
+
+    it "cannot be parsed from a boolean" do
+      assert :error == parse(:naive_datetime, true)
+      assert :error == parse(:naive_datetime, false)
+    end
+  end
+
+  describe ":date" do
+    it "serializes as an ISO8601 date string" do
+      assert "2017-01-27" == serialize(:date, @date)
+    end
+
+    it "can be parsed from an ISO8601 date string" do
+      assert {:ok, @date} == parse(:date, "2017-01-27")
+    end
+
+    it "cannot be parsed when time is included" do
+      assert :error == parse(:date, "2017-01-27T20:31:55Z")
+      assert :error == parse(:date, "2017-01-27 20:31:55Z")
+      assert :error == parse(:date, "2017-01-27 20:31:55")
+    end
+
+    it "cannot be parsed when date is missing" do
+      assert :error == parse(:date, "20:31:55")
+    end
+
+    it "cannot be parsed from a binary not formatted according to ISO8601" do
+      assert :error == parse(:date, "abc123")
+      assert :error == parse(:date, "01/25/2017 20:31:55")
+      assert :error == parse(:date, "2017-15-42T31:71:95Z")
+    end
+
+    it "cannot be parsed from a number" do
+      assert :error == parse(:date, 0)
+      assert :error == parse(:date, 0.0)
+    end
+
+    it "cannot be parsed from a boolean" do
+      assert :error == parse(:date, true)
+      assert :error == parse(:date, false)
+    end
+  end
+
+  describe ":time" do
+    it "serializes as an ISO8601 time string" do
+      assert "20:31:55" == serialize(:time, @time)
+    end
+
+    it "can be parsed from an ISO8601 date string" do
+      assert {:ok, @time} == parse(:time, "20:31:55")
+    end
+
+    it "cannot be parsed when date is included" do
+      assert :error == parse(:time, "2017-01-27T20:31:55Z")
+      assert :error == parse(:time, "2017-01-27 20:31:55Z")
+      assert :error == parse(:time, "2017-01-27 20:31:55")
+    end
+
+    it "cannot be parsed when time is missing" do
+      assert :error == parse(:time, "2017-01-27")
+    end
+
+    it "cannot be parsed from a binary not formatted according to ISO8601" do
+      assert :error == parse(:time, "abc123")
+      assert :error == parse(:time, "01/25/2017 20:31:55")
+      assert :error == parse(:time, "2017-15-42T31:71:95Z")
+    end
+
+    it "cannot be parsed from a number" do
+      assert :error == parse(:time, 0)
+      assert :error == parse(:time, 0.0)
+    end
+
+    it "cannot be parsed from a boolean" do
+      assert :error == parse(:time, true)
+      assert :error == parse(:time, false)
+    end
+  end
+end
