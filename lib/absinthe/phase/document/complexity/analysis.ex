64acfa416dba5758d0a6d9ce4fbf13db6faf3dc7
James Fish <james@fishcakez.com>
2017-02-04 22:36:20 +0000

    Move complexity error handling to result phase

--- lib/absinthe/phase/document/complexity/analysis.ex
+++ lib/absinthe/phase/document/complexity/analysis.ex
@@ -15,23 +15,22 @@ defmodule Absinthe.Phase.Document.Complexity.Analysis do
   @spec run(Blueprint.t, Keyword.t) :: Phase.result_t
   def run(input, options \\ []) do
     info_data = info_boilerplate(input, options)
-    max = Keyword.get(options, :max_complexity, :infinity)
-    fun = &handle_node(&1, info_data, max)
+    fun = &handle_node(&1, info_data)
     {:ok, Blueprint.update_current(input, &Blueprint.postwalk(&1, fun))}
   end
 
   def handle_node(%Blueprint.Document.Field{fields: fields,
                                             argument_data: args,
-                                            schema_node: schema_node} = node, info_data, max) do
+                                            schema_node: schema_node} = node, info_data) do
     child_complexity = sum_complexity(fields)
     complexity = field_complexity(schema_node, args, child_complexity, info_data, node)
-    check_complexity(%{node | complexity: complexity}, complexity, max)
+    %{node | complexity: complexity}
   end
-  def handle_node(%Blueprint.Document.Operation{complexity: nil, fields: fields} = node, _, max) do
+  def handle_node(%Blueprint.Document.Operation{complexity: nil, fields: fields} = node, _) do
     complexity = sum_complexity(fields)
-    check_complexity(%{node | complexity: complexity}, complexity, max)
+    %{node | complexity: complexity}
   end
-  def handle_node(node, _, _) do
+  def handle_node(node, _) do
     node
   end
 
@@ -58,27 +57,6 @@ defmodule Absinthe.Phase.Document.Complexity.Analysis do
 
   defp sum_complexity(%{complexity: complexity}, acc) do
     complexity + acc
-  end
-
-  defp check_complexity(node, complexity, max) when complexity > max do
-    node
-    |> flag_invalid(:too_complex)
-    |> put_error(error(node, complexity, max))
-  end
-  defp check_complexity(node, _, _) do
-    node
-  end
-
-  defp error(%{name: name, source_location: location}, complexity, max) do
-    Phase.Error.new(
-      __MODULE__,
-      error_message(name, complexity, max),
-      location: location
-    )
-  end
-
-  def error_message(name, complexity, max) do
-    "#{name} is too complex: complexity is #{complexity} and maximum is #{max}"
   end
 
   # Execution context data that's common to all fields
