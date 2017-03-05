57d86abf329298b2aa41725744699d07e3d7c7cd
Mickael Lucchini <hello@mickael.lu>
2017-03-05 08:37:16 -0500

    bumped elixir version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Absinthe.Mixfile do
   def project do
     [app: :absinthe,
      version: @version,
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
