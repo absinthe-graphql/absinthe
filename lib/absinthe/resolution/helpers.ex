6b21fd91f0470c6daf3b64e680b128d932a139e8
Ben Wilson <benwilson512@gmail.com>
2017-04-25 07:33:57 -0400

    doc fixes

--- lib/absinthe/resolution/helpers.ex
+++ lib/absinthe/resolution/helpers.ex
@@ -30,19 +30,20 @@ defmodule Absinthe.Resolution.Helpers do
   Raw usage:
   ```elixir
   object :post do
-    field :title, :string
-    field :body, :string
+    field :name, :string
     field :author, :user do
       resolve fn post, _, _ ->
-        batch({EctoBatch, :by_id}, {User, post.author_id}, fn batch_results ->
-          {:ok, batch_results[User][post.author_id]}
+        batch({__MODULE__, :users_by_id}, post.author_id, fn batch_results ->
+          {:ok, Map.get(batch_results, post.author_id)}
         end)
       end
     end
   end
-  field
-  resolve fn post, _, _
-  batch({EctoBatch, :by_id}, [])
+
+  def users_by_id(_, user_ids) do
+    users = Repo.all from u in User, where: u.id in ^user_ids
+    Map.new(users, fn user -> {user.id, user} end)
+  end
   ```
   """
   @spec batch(Middleware.Batch.batch_fun, term, Middleware.Batch.post_batch_fun) :: {:plugin, Middleware.Batch, term}
