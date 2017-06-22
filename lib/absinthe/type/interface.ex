a67947aa306a60416823a7747a6dc6d30477e818
Jason Chen <jhchen7@gmail.com>
2017-06-22 12:26:37 -0700

    small documentation fix
    example mixed different arities

--- lib/absinthe/type/interface.ex
+++ lib/absinthe/type/interface.ex
@@ -23,7 +23,7 @@ defmodule Absinthe.Type.Interface do
     resolve_type fn
       %{age: _}, _ -> :person
       %{employee_count: _}, _ -> :business
-      _ -> nil
+      _, _ -> nil
     end
   end
 
