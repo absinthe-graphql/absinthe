3044cdf11f61b6ae24f8fec1ff7a8581244aaad8
Ben Wilson <benwilson512@gmail.com>
2018-02-20 15:07:04 -0500

    improve schema compile time

--- lib/absinthe/schema/notation/experimental.ex
+++ lib/absinthe/schema/notation/experimental.ex
@@ -3,51 +3,6 @@ defmodule Absinthe.Schema.Notation.Experimental do
 
   alias Absinthe.Blueprint
 
-  @typep scope_t :: {:type, atom} | {:field, {:type, atom}, atom}
-
-  @spec update_type(Blueprint.t, atom, (Blueprint.Schema.type_t -> Blueprint.Schema.type_t)) :: Blueprint.t
-  @doc false
-  def update_type(blueprint, identifier, fun) do
-    update_in(blueprint, [Access.key(:types), Access.all()], fn
-      %{identifier: ^identifier} = type ->
-        fun.(type)
-      other ->
-        other
-    end)
-  end
-
-  @spec put_type(Blueprint.t, Blueprint.Schema.type_t) :: Blueprint.t
-  @doc false
-  def put_type(blueprint, type) do
-    update_in(blueprint.types, &[type | &1])
-  end
-
-  @spec put_field(Blueprint.t, scope_t, Blueprint.Schema.FieldDefinition.t) :: Blueprint.t
-  @doc false
-  def put_field(blueprint, {:type, identifier}, %Blueprint.Schema.FieldDefinition{} = field) do
-    update_type(blueprint, identifier, fn
-      type ->
-        update_in(type.fields, &[field | &1])
-    end)
-  end
-
-  @spec put_attrs(Blueprint.t, scope_t, Keyword.t) :: Blueprint.t
-  @doc false
-  def put_attrs(blueprint, {:type, type_identifier}, attrs) do
-    update_type(blueprint, type_identifier, &struct(&1, attrs))
-  end
-  def put_attrs(blueprint, {:field, {:type, type_identifier}, field_identifier}, attrs) do
-    update_type(blueprint, type_identifier, fn
-      type ->
-        update_in(type, [Access.key(:fields), Access.all()], fn
-          %{identifier: ^field_identifier} = field ->
-            struct(field, attrs)
-          other ->
-            other
-        end)
-    end)
-  end
-
   @spec concat_types(Blueprint.t, Blueprint.t, Keyword.t) :: Blueprint.t
   def concat_types(blueprint, other, opts) do
     selected = select_import(other.types, Map.new(opts))
@@ -75,30 +30,17 @@ defmodule Absinthe.Schema.Notation.Experimental do
 
   defp do_concat_fields(%{fields: fields}, blueprint, scope, _criteria, opts) do
     selected = select_import(fields, Map.new(opts))
-    Enum.reduce(selected, blueprint, &put_field(&2, scope, &1))
+    # Enum.reduce(selected, blueprint, &put_field(&2, scope, &1))
   end
   defp do_concat_fields(_, _, _, criteria, _) do
     raise "Not a valid source for fields: #{inspect(criteria)}"
   end
 
-  defp push_scope(scope) do
-    quote do: @absinthe_scopes [unquote(scope) | @absinthe_scopes]
-  end
-  defp pop_scope do
-    quote do: @absinthe_scopes tl(@absinthe_scopes)
-  end
-  defp scoped(body, scope) do
-    [
-      push_scope(scope),
-      body,
-      pop_scope()
-    ]
-  end
-
   defmacro __using__(_opts) do
+    Module.register_attribute(__CALLER__.module, :absinthe_blueprint, [])
+    Module.put_attribute(__CALLER__.module, :absinthe_blueprint, [%Absinthe.Blueprint{}])
+    Module.register_attribute(__CALLER__.module, :absinthe_desc, [accumulate: true])
     quote do
-      @absinthe_blueprint %Absinthe.Blueprint{}
-      @absinthe_scopes []
       @desc nil
       import unquote(__MODULE__), only: :macros
       @before_compile unquote(__MODULE__)
@@ -106,54 +48,42 @@ defmodule Absinthe.Schema.Notation.Experimental do
   end
 
   defmacro query(do: body) do
-    object_definition(:query, [name: "RootQueryType"], body)
+    object_definition(__CALLER__, :query, [name: "RootQueryType"], body)
   end
   defmacro query(attrs, do: body) when is_list(attrs) do
-    object_definition(:query, attrs, body)
+    object_definition(__CALLER__, :query, attrs, body)
   end
 
   defmacro mutation(do: body) do
-    object_definition(:mutation, [name: "RootMutationType"], body)
+    object_definition(__CALLER__, :mutation, [name: "RootMutationType"], body)
   end
   defmacro mutation(attrs, do: body) when is_list(attrs) do
-    object_definition(:mutation, attrs, body)
+    object_definition(__CALLER__, :mutation, attrs, body)
   end
 
   defmacro subscription(do: body) do
-    object_definition(:subscription, [name: "RootSubscriptionType"], body)
+    object_definition(__CALLER__, :subscription, [name: "RootSubscriptionType"], body)
   end
   defmacro subscription(attrs, do: body) when is_list(attrs) do
-    object_definition(:subscription, attrs, body)
+    object_definition(__CALLER__, :subscription, attrs, body)
   end
 
   defmacro object(identifier, do: body) do
-    object_definition(identifier, [], body)
+    object_definition(__CALLER__, identifier, [], body)
   end
 
   defmacro object(identifier, attrs, do: body) do
-    object_definition(identifier, attrs, body)
+    object_definition(__CALLER__, identifier, attrs, body)
   end
 
-  def object_definition(identifier, attrs, body) do
-    {desc, attrs} =
-      attrs
-      |> Keyword.put_new(:name, default_object_name(identifier))
-      |> Keyword.pop(:description)
+  defp put_attr(module, thing) do
+    existing = Module.get_attribute(module, :absinthe_blueprint)
+    Module.put_attribute(module, :absinthe_blueprint, [thing | existing])
+  end
 
-    [
-      quote do
-        @absinthe_blueprint unquote(__MODULE__).put_type(
-          @absinthe_blueprint,
-          %Absinthe.Blueprint.Schema.ObjectTypeDefinition{
-            unquote_splicing(attrs),
-            description: @desc || unquote(desc),
-            identifier: unquote(identifier)
-          }
-        )
-        @desc nil
-      end,
-      body |> scoped({:type, identifier}),
-    ]
+  defmacro close_scope() do
+    put_attr(__CALLER__.module, :close)
+    []
   end
 
   defp default_object_name(identifier) do
@@ -176,54 +106,62 @@ defmodule Absinthe.Schema.Notation.Experimental do
   @spec import_fields(atom | {module, atom}, Keyword.t) :: Macro.t
   defmacro import_fields(source_criteria, opts \\ []) do
     quote do
-      @absinthe_blueprint unquote(__MODULE__).concat_fields(
-        @absinthe_blueprint,
-        hd(@absinthe_scopes),
-        unquote(source_criteria),
-        unquote(opts)
-      )
     end
   end
 
   @spec field(atom, atom | Keyword.t) :: Macro.t
   defmacro field(identifier, attrs) when is_list(attrs) do
-    field_definition(identifier, attrs, nil)
+    field_definition(__CALLER__, identifier, attrs, nil)
   end
   defmacro field(identifier, type) when is_atom(type) do
-    field_definition(identifier, [type: type], nil)
+    field_definition(__CALLER__, identifier, [type: type], nil)
   end
 
   @spec field(atom, atom | Keyword.t, [do: Macro.t]) :: Macro.t
   defmacro field(identifier, attrs, do: body) when is_list(attrs) do
-    field_definition(identifier, attrs, body)
+    field_definition(__CALLER__, identifier, attrs, body)
   end
   defmacro field(identifier, type, do: body) when is_atom(type) do
-    field_definition(identifier, [type: type], body)
+    field_definition(__CALLER__, identifier, [type: type], body)
   end
 
-  @spec field_definition(atom, Keyword.t, Macro.t) :: Macro.t
-  def field_definition(identifier, attrs, body) do
-    {desc, attrs} =
+  def field_definition(caller, identifier, attrs, body) do
+    attrs =
       attrs
+      |> Keyword.put(:identifier, identifier)
       |> Keyword.put_new(:name, default_field_name(identifier))
-      |> Keyword.pop(:description)
+
+    field = struct(Absinthe.Blueprint.Schema.FieldDefinition, attrs)
+
+    put_attr(caller.module, field)
 
     [
       quote do
-        @absinthe_blueprint unquote(__MODULE__).put_field(
-          @absinthe_blueprint,
-          hd(@absinthe_scopes),
-          %Absinthe.Blueprint.Schema.FieldDefinition{
-            unquote_splicing(attrs),
-            description: @desc || unquote(desc),
-            identifier: unquote(identifier),
-          }
-        )
-        @absinthe_scopes [{:field, hd(@absinthe_scopes), unquote(identifier)} | @absinthe_scopes]
+        Module.put_attribute(__MODULE__, :absinthe_desc, {unquote(identifier), @desc})
+        @desc nil
+      end,
+      [],
+      quote do: unquote(__MODULE__).close_scope()
+    ]
+  end
+
+  def object_definition(caller, identifier, attrs, body) do
+    attrs =
+      attrs
+      |> Keyword.put(:identifier, identifier)
+      |> Keyword.put_new(:name, default_object_name(identifier))
+
+    object = struct(Absinthe.Blueprint.Schema.ObjectTypeDefinition, attrs)
+
+    put_attr(caller.module, object)
+
+    [
+      quote do
+        @absinthe_desc {:desc, @desc}
         @desc nil
       end,
       body,
-      pop_scope()
+      quote do: unquote(__MODULE__).close_scope()
     ]
   end
 
@@ -241,20 +179,35 @@ defmodule Absinthe.Schema.Notation.Experimental do
 
   defmacro middleware(module, opts) do
     quote do
-      @absinthe_blueprint unquote(__MODULE__).put_attrs(
-        @absinthe_blueprint,
-        hd(@absinthe_scopes),
-        middleware: [{unquote(module), unquote(Macro.escape(opts))}]
-      )
+      # @absinthe_blueprint unquote(__MODULE__).put_attrs(
+      #   @absinthe_blueprint,
+      #   hd(@absinthe_scopes),
+      #   middleware: [{unquote(module), unquote(Macro.escape(opts))}]
+      # )
     end
   end
 
-  defmacro __before_compile__(_env) do
+  defmacro __before_compile__(env) do
+    attrs =
+      env.module
+      |> Module.get_attribute(:absinthe_blueprint)
+      |> Enum.reverse
+    blueprint = build_blueprint(attrs)
     quote do
       def __absinthe_blueprint__ do
-        @absinthe_blueprint
+        unquote(Macro.escape(blueprint))
       end
     end
+  end
+
+  alias Absinthe.Blueprint.Schema
+  defp build_blueprint([%Absinthe.Blueprint{} = bp | attrs]) do
+    types = build_types(attrs, [], %{types: []})
+    Map.merge(bp, types)
+  end
+
+  defp build_types([%Schema.ObjectTypeDefinition{} = obj | rest], tmp, finished) do
+    finished
   end
 
 end
