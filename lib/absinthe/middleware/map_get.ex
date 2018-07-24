aea13ab54e255aa61b2d787b633c9241cfee3577
Jason Axelson <axelson@users.noreply.github.com>
2018-07-24 12:30:14 -1000

    Default MapGet middleware should ignore already resolved fields (#582)
    * Default MapGet middleware should ignore already resolved fields
    
    Similar to the default resolve middleware `Absinthe.Middleware.MapGet`
    should ignore already resolved fields instead of over-writing their
    values.
    
    * Update moduledoc of MapGet

--- lib/absinthe/middleware/map_get.ex
+++ lib/absinthe/middleware/map_get.ex
@@ -1,11 +1,18 @@
 defmodule Absinthe.Middleware.MapGet do
   @moduledoc """
-  This is the default middleware.
+  This is the default middleware. It assumes the the object it receives is a map
+  and uses `Map.get/2` to get the value for this field. If this field is already
+  marked as resolved, then this middleware does not touch it.
+
+  If you want to replace this middleware you should use
+  `Absinthe.Schema.replace_default/4`
   """
 
   @behaviour Absinthe.Middleware
 
-  def call(%{source: source} = res, key) do
+  def call(%{state: :unresolved, source: source} = res, key) do
     %{res | state: :resolved, value: Map.get(source, key)}
   end
+
+  def call(res, _key), do: res
 end
