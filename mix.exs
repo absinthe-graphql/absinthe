7b600c2fd40cdc1115734ed9a82e92ea14eb1344
Unknown <jeanluc.geering@ufirstgroup.com>
2018-08-05 08:10:55 +0200

    update package links
    - add link to changelog
    - add link to website
    - better link name (github -> GitHub)

--- mix.exs
+++ mix.exs
@@ -37,7 +37,11 @@ defmodule Absinthe.Mixfile do
         "Ben Wilson"
       ],
       licenses: ["MIT"],
-      links: %{github: "https://github.com/absinthe-graphql/absinthe"}
+      links: %{
+        "Website": "https://absinthe-graphql.org",
+        "Changelog": "https://github.com/absinthe-graphql/absinthe/blob/master/CHANGELOG.md", 
+        "GitHub": "https://github.com/absinthe-graphql/absinthe"
+      }
     ]
   end
 
