0c3a07a3cf1c80360f4cd630fef77cfa944cf3e4
Ben Wilson <benwilson512@gmail.com>
2018-08-23 09:57:04 -0400

    mark some probably irrelevant tests pending

--- test/absinthe/pipeline_test.exs
+++ test/absinthe/pipeline_test.exs
@@ -31,11 +31,12 @@ defmodule Absinthe.PipelineTest do
       name: String!
     }
     """
-
+    @tag :pending
     test "can create a blueprint without a prototype schema" do
       assert {:ok, %Blueprint{}, _} = Pipeline.run(@query, Pipeline.for_schema(nil))
     end
 
+    @tag :pending
     test "can create a blueprint with a prototype schema" do
       assert {:ok, %Blueprint{}, _} = Pipeline.run(@query, Pipeline.for_schema(Schema))
     end
