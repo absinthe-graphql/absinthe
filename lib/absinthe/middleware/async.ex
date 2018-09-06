aea8e90a81e4f0246be3ccd0184fdf7c8365cf1e
Jeff Weiss <jeffweiss@users.noreply.github.com>
2018-09-05 17:50:04 -0700

    fix documentation typo (#604)
    Prior to this commit, the explanatory text about the async middleware
    mispelled `reason` as `reasoon`.

--- lib/absinthe/middleware/async.ex
+++ lib/absinthe/middleware/async.ex
@@ -68,7 +68,7 @@ defmodule Absinthe.Middleware.Async do
   # we handle the different tuple results.
   #
   # The `put_result` function handles setting the appropriate state.
-  # If the result is an `{:ok, value} | {:error, reasoon}` tuple it will set
+  # If the result is an `{:ok, value} | {:error, reason}` tuple it will set
   # the state to `:resolved`, and if it is another middleware tuple it will
   # set the state to unresolved.
   def call(%{state: :suspended} = res, {task, opts}) do
