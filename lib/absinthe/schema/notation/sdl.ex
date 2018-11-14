633dd447ec15ebb06c7c1b92a11d91fc1e38bf4e
Bruce Williams <brwcodes@gmail.com>
2018-11-13 22:39:34 -0800

    Basic ref handling

--- lib/absinthe/schema/notation/sdl.ex
+++ lib/absinthe/schema/notation/sdl.ex
@@ -4,13 +4,14 @@ defmodule Absinthe.Schema.Notation.SDL do
   @doc """
   Parse definitions from SDL source
   """
-  @spec parse(sdl :: String.t(), Module.t()) ::
+  @spec parse(sdl :: String.t(), Module.t(), map())  ::
           {:ok, [Absinthe.Blueprint.Schema.type_t()]} | {:error, String.t()}
-  def parse(sdl, module) do
+  def parse(sdl, module, ref) do
     with {:ok, doc} <- Absinthe.Phase.Parse.run(sdl) do
       definitions =
         doc.input.definitions
         |> Enum.map(&Absinthe.Blueprint.Draft.convert(&1, doc))
+        |> Enum.map(&put_ref(&1, ref))
         |> Enum.map(fn type -> %{type | module: module} end)
 
       {:ok, definitions}
@@ -27,4 +28,22 @@ defmodule Absinthe.Schema.Notation.SDL do
         other
     end
   end
+
+  defp put_ref(%{fields: fields} = node, ref) do
+    %{node | fields: Enum.map(fields, &put_ref(&1, ref))}
+    |> do_put_ref(ref)
+  end
+  defp put_ref(%{arguments: args} = node, ref) do
+    %{node | arguments: Enum.map(args, &put_ref(&1, ref))}
+    |> do_put_ref(ref)
+  end
+  defp put_ref(%{directives: dirs} = node, ref) do
+    %{node | directives: Enum.map(dirs, &put_ref(&1, ref))}
+    |> do_put_ref(ref)
+  end
+  defp put_ref(node, ref), do: do_put_ref(node, ref)
+
+  defp do_put_ref(%{__reference__: nil} = node, ref), do: Map.put(node, :__reference__, ref)
+  defp do_put_ref(node, _ref), do: node
+
 end
