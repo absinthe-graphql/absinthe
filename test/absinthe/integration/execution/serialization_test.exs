c3a5bcc70bc265a56fccfc7f038158ff1e10386c
Ben Wilson <benwilson512@gmail.com>
2019-09-06 09:18:22 -0400

    add forgotten test file

---
+++ test/absinthe/integration/execution/serialization_test.exs
@@ -0,0 +1,48 @@
+defmodule Absinthe.Integration.Execution.SerializationTest do
+  use Absinthe.Case, async: true
+
+  defmodule Schema do
+    use Absinthe.Schema
+
+    query do
+      field :bad_integer, :integer do
+        resolve fn _, _, _ -> {:ok, 1.0} end
+      end
+
+      field :bad_float, :float do
+        resolve fn _, _, _ -> {:ok, 1} end
+      end
+
+      field :bad_boolean, :boolean do
+        resolve fn _, _, _ -> {:ok, "true"} end
+      end
+    end
+  end
+
+  @query """
+  query { badInteger }
+  """
+  test "returning not an integer for an integer raises" do
+    assert_raise(Absinthe.SerializationError, fn ->
+      Absinthe.run(@query, Schema)
+    end)
+  end
+
+  @query """
+  query { badFloat }
+  """
+  test "returning not a float for a float raises" do
+    assert_raise(Absinthe.SerializationError, fn ->
+      Absinthe.run(@query, Schema)
+    end)
+  end
+
+  @query """
+  query { badBoolean }
+  """
+  test "returning not a boolean for a boolean raises" do
+    assert_raise(Absinthe.SerializationError, fn ->
+      Absinthe.run(@query, Schema)
+    end)
+  end
+end
