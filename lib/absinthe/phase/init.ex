73b8281cc8eae076566d7c457e4152b90c742514
Ben Wilson <benwilson512@gmail.com>
2019-09-07 09:34:07 -0400

    remove warning

--- lib/absinthe/phase/init.ex
+++ lib/absinthe/phase/init.ex
@@ -6,7 +6,7 @@ defmodule Absinthe.Phase.Init do
   alias Absinthe.{Blueprint, Language, Phase}
 
   @spec run(String.t() | Language.Source.t() | Blueprint.t(), Keyword.t()) :: Phase.result_t()
-  def run(input, options \\ []) do
+  def run(input, _options \\ []) do
     {:record_phases, make_blueprint(input),
      fn bp, phases ->
        %{bp | initial_phases: phases}
