bd998d6018f0c526cd10ba369f6b5460ccee46fd
Thiago Santos <thia.mdossantos@gmail.com>
2019-11-29 13:18:20 -0300

    Use node_name/0 to check the node origin

--- lib/absinthe/subscription/proxy.ex
+++ lib/absinthe/subscription/proxy.ex
@@ -20,23 +20,21 @@ defmodule Absinthe.Subscription.Proxy do
     {:ok, %__MODULE__{pubsub: pubsub}}
   end
 
-  def handle_info(%{node: src_node}, state) when src_node == node() do
-    {:noreply, state}
-  end
-
   def handle_info(payload, state) do
     # There's no meaningful form of backpressure to have here, and we can't
     # bottleneck execution inside each proxy process
 
-    # TODO: This should maybe be supervised? I feel like the linking here isn't
-    # what it should be.
-    Task.start_link(fn ->
-      Subscription.Local.publish_mutation(
-        state.pubsub,
-        payload.mutation_result,
-        payload.subscribed_fields
-      )
-    end)
+    unless payload.node == state.pubsub.node_name() do
+      # TODO: This should maybe be supervised? I feel like the linking here isn't
+      # what it should be.
+      Task.start_link(fn ->
+        Subscription.Local.publish_mutation(
+          state.pubsub,
+          payload.mutation_result,
+          payload.subscribed_fields
+        )
+      end)
+    end
 
     {:noreply, state}
   end
