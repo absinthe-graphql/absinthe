f646a3b02d4387d112d17797841643024da2c3fa
Vince Foley <vincefoley@gmail.com>
2020-01-29 17:24:21 -0800

    keyword extend not yet supported

--- lib/absinthe/language/type_extension_definition.ex
+++ lib/absinthe/language/type_extension_definition.ex
@@ -1,7 +1,7 @@
 defmodule Absinthe.Language.TypeExtensionDefinition do
   @moduledoc false
 
-  alias Absinthe.Language
+  alias Absinthe.{Language, Blueprint}
 
   defstruct definition: nil,
             loc: %{line: nil}
@@ -10,4 +10,16 @@ defmodule Absinthe.Language.TypeExtensionDefinition do
           definition: Language.ObjectTypeDefinition.t(),
           loc: Language.loc_t()
         }
+
+  defimpl Blueprint.Draft do
+    def convert(node, _doc) do
+      raise Absinthe.Schema.Notation.Error,
+            """
+            \n
+            SDL Compilation failed:
+            -----------------------
+            Keyword `extend` is not yet supported (#{inspect(node.loc)})
+            """
+    end
+  end
 end
