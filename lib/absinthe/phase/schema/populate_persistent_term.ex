6d6d36f8df0e49dd6b58dd32e3ea18e471fe4873
Ben Wilson <benwilson512@gmail.com>
2020-03-29 12:58:21 -0400

    conditionally compile depending on persistent_term availability

--- lib/absinthe/phase/schema/populate_persistent_term.ex
+++ lib/absinthe/phase/schema/populate_persistent_term.ex
@@ -1,81 +1,89 @@
-defmodule Absinthe.Phase.Schema.PopulatePersistentTerm do
-  @moduledoc false
+if Code.ensure_loaded?(:persistent_term) do
+  defmodule Absinthe.Phase.Schema.PopulatePersistentTerm do
+    @moduledoc false
 
-  alias Absinthe.Blueprint.Schema
+    alias Absinthe.Blueprint.Schema
 
-  def run(blueprint, opts) do
-    %{schema_definitions: [schema]} = blueprint
+    def run(blueprint, opts) do
+      %{schema_definitions: [schema]} = blueprint
 
-    type_list =
-      Map.new(schema.type_definitions, fn type_def ->
-        {type_def.identifier, type_def.name}
+      type_list =
+        Map.new(schema.type_definitions, fn type_def ->
+          {type_def.identifier, type_def.name}
+        end)
+
+      types_map =
+        schema.type_artifacts
+        |> Enum.flat_map(fn type -> [{type.identifier, type}, {type.name, type}] end)
+        |> Map.new()
+
+      referenced_types =
+        for type_def <- schema.type_definitions,
+            type_def.__private__[:__absinthe_referenced__],
+            into: %{},
+            do: {type_def.identifier, type_def.name}
+
+      directive_list =
+        Map.new(schema.directive_definitions, fn type_def ->
+          {type_def.identifier, type_def.name}
+        end)
+
+      directives_map =
+        schema.directive_artifacts
+        |> Enum.flat_map(fn type -> [{type.identifier, type}, {type.name, type}] end)
+        |> Map.new()
+
+      prototype_schema = Keyword.fetch!(opts, :prototype_schema)
+
+      metadata = build_metadata(schema)
+
+      implementors = build_implementors(schema)
+
+      schema_content = %{
+        __absinthe_types__: %{
+          referenced: referenced_types,
+          all: type_list
+        },
+        __absinthe_directives__: directive_list,
+        __absinthe_interface_implementors__: implementors,
+        __absinthe_prototype_schema__: prototype_schema,
+        __absinthe_type__: types_map,
+        __absinthe_directive__: directives_map,
+        __absinthe_reference__: metadata
+      }
+
+      # :erts_debug.flat_size(schema) |> IO.inspect(label: :flat_size)
+      schema_name = opts[:schema] || raise "no schema name provided"
+
+      :persistent_term.put(
+        {Absinthe.Schema.PersistentTerm, schema_name},
+        schema_content
+      )
+
+      {:ok, blueprint}
+    end
+
+    def build_metadata(schema) do
+      for type <- schema.type_definitions do
+        {type.identifier, type.__reference__}
+      end
+    end
+
+    defp build_implementors(schema) do
+      schema.type_definitions
+      |> Enum.filter(&match?(%Schema.InterfaceTypeDefinition{}, &1))
+      |> Map.new(fn iface ->
+        implementors =
+          Schema.InterfaceTypeDefinition.find_implementors(iface, schema.type_definitions)
+
+        {iface.identifier, Enum.sort(implementors)}
       end)
-
-    types_map =
-      schema.type_artifacts
-      |> Enum.flat_map(fn type -> [{type.identifier, type}, {type.name, type}] end)
-      |> Map.new()
-
-    referenced_types =
-      for type_def <- schema.type_definitions,
-          type_def.__private__[:__absinthe_referenced__],
-          into: %{},
-          do: {type_def.identifier, type_def.name}
-
-    directive_list =
-      Map.new(schema.directive_definitions, fn type_def ->
-        {type_def.identifier, type_def.name}
-      end)
-
-    directives_map =
-      schema.directive_artifacts
-      |> Enum.flat_map(fn type -> [{type.identifier, type}, {type.name, type}] end)
-      |> Map.new()
-
-    prototype_schema = Keyword.fetch!(opts, :prototype_schema)
-
-    metadata = build_metadata(schema)
-
-    implementors = build_implementors(schema)
-
-    schema_content = %{
-      __absinthe_types__: %{
-        referenced: referenced_types,
-        all: type_list
-      },
-      __absinthe_directives__: directive_list,
-      __absinthe_interface_implementors__: implementors,
-      __absinthe_prototype_schema__: prototype_schema,
-      __absinthe_type__: types_map,
-      __absinthe_directive__: directives_map,
-      __absinthe_reference__: metadata
-    }
-
-    # :erts_debug.flat_size(schema) |> IO.inspect(label: :flat_size)
-    schema_name = opts[:schema] || raise "no schema name provided"
-
-    :persistent_term.put(
-      {Absinthe.Schema.PersistentTerm, schema_name},
-      schema_content
-    )
-
-    {:ok, blueprint}
-  end
-
-  def build_metadata(schema) do
-    for type <- schema.type_definitions do
-      {type.identifier, type.__reference__}
     end
   end
-
-  defp build_implementors(schema) do
-    schema.type_definitions
-    |> Enum.filter(&match?(%Schema.InterfaceTypeDefinition{}, &1))
-    |> Map.new(fn iface ->
-      implementors =
-        Schema.InterfaceTypeDefinition.find_implementors(iface, schema.type_definitions)
-
-      {iface.identifier, Enum.sort(implementors)}
-    end)
+else
+  defmodule Absinthe.Phase.Schema.PopulatePersistentTerm do
+    def run(_, _) do
+      raise "This phase requires OTP >= 22"
+    end
   end
 end
--- lib/absinthe/schema/persistent_term.ex
+++ lib/absinthe/schema/persistent_term.ex
@@ -1,63 +1,79 @@
-defmodule Absinthe.Schema.PersistentTerm do
-  @moduledoc false
+if Code.ensure_loaded?(:persistent_term) do
+  defmodule Absinthe.Schema.PersistentTerm do
+    @moduledoc false
 
-  def pipeline(pipeline) do
-    Enum.map(pipeline, fn
-      {Absinthe.Phase.Schema.Compile, options} ->
-        {Absinthe.Phase.Schema.PopulatePersistentTerm, options}
+    def pipeline(pipeline) do
+      Enum.map(pipeline, fn
+        {Absinthe.Phase.Schema.Compile, options} ->
+          {Absinthe.Phase.Schema.PopulatePersistentTerm, options}
 
-      phase ->
-        phase
-    end)
+        phase ->
+          phase
+      end)
+    end
+
+    def __absinthe_type__(schema_mod, name) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_type__)
+      |> Map.get(name)
+    end
+
+    def __absinthe_directive__(schema_mod, name) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_directive__)
+      |> Map.get(name)
+    end
+
+    def __absinthe_types__(schema_mod) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_types__)
+      |> Map.fetch!(:referenced)
+    end
+
+    def __absinthe_types__(schema_mod, group) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_types__)
+      |> Map.fetch!(group)
+    end
+
+    def __absinthe_directives__(schema_mod) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_directives__)
+    end
+
+    def __absinthe_interface_implementors__(schema_mod) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_interface_implementors__)
+    end
+
+    def __absinthe_prototype_schema__(schema_mod) do
+      schema_mod
+      |> get()
+      |> Map.fetch!(:__absinthe_prototype_schema__)
+    end
+
+    defp get(schema) do
+      :persistent_term.get({__MODULE__, schema})
+    end
   end
+else
+  defmodule Absinthe.Schema.PersistentTerm do
+    @moduledoc false
 
-  def __absinthe_type__(schema_mod, name) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_type__)
-    |> Map.get(name)
-  end
+    def pipeline(_), do: raise(@error)
 
-  def __absinthe_directive__(schema_mod, name) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_directive__)
-    |> Map.get(name)
-  end
-
-  def __absinthe_types__(schema_mod) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_types__)
-    |> Map.fetch!(:referenced)
-  end
-
-  def __absinthe_types__(schema_mod, group) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_types__)
-    |> Map.fetch!(group)
-  end
-
-  def __absinthe_directives__(schema_mod) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_directives__)
-  end
-
-  def __absinthe_interface_implementors__(schema_mod) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_interface_implementors__)
-  end
-
-  def __absinthe_prototype_schema__(schema_mod) do
-    schema_mod
-    |> get()
-    |> Map.fetch!(:__absinthe_prototype_schema__)
-  end
-
-  defp get(schema) do
-    :persistent_term.get({__MODULE__, schema})
+    def __absinthe_type__(_, _), do: raise(@error)
+    def __absinthe_directive__(_, _), do: raise(@error)
+    def __absinthe_types__(_), do: raise(@error)
+    def __absinthe_types__(_, _), do: raise(@error)
+    def __absinthe_directives__(_), do: raise(@error)
+    def __absinthe_interface_implementors__(_), do: raise(@error)
+    def __absinthe_prototype_schema__(), do: raise(@error)
   end
 end
