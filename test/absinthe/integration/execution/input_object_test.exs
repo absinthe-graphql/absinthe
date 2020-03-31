392426bd550fc65b4b0304d01813229ac4daaba6
Ben Wilson <benwilson512@gmail.com>
2020-03-31 12:55:43 -0400

    test case validating that #555 is fixed

--- test/absinthe/integration/execution/input_object_test.exs
+++ test/absinthe/integration/execution/input_object_test.exs
@@ -14,4 +14,28 @@ defmodule Elixir.Absinthe.Integration.Execution.InputObjectTest do
     assert {:ok, %{data: %{"updateThing" => %{"name" => "Foo", "value" => 100}}}} ==
              Absinthe.run(@query, Absinthe.Fixtures.Things.MacroSchema, [])
   end
+
+  @query """
+  mutation ($input: Boolean) {
+    updateThing(id: "foo", thing: $input) {
+      name
+      value
+    }
+  }
+  """
+
+  test "errors if an invalid type is passed" do
+    assert {:ok,
+            %{
+              errors: [
+                %{
+                  locations: [%{column: 26, line: 2}],
+                  message: "Argument \"thing\" has invalid value $input."
+                }
+              ]
+            }} ==
+             Absinthe.run(@query, Absinthe.Fixtures.Things.MacroSchema,
+               variables: %{"input" => true}
+             )
+  end
 end
