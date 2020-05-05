bce96a2ce0152a31dc2dca7bcca9f9ee1b2f972b
William Lanchantin <william.lanchantin@cargosense.com>
2020-05-05 14:18:00 -0500

    make relevant tests pass

--- test/absinthe/phase/document/validation/variables_of_correct_type_test.exs
+++ test/absinthe/phase/document/validation/variables_of_correct_type_test.exs
@@ -3,79 +3,61 @@ defmodule Absinthe.Phase.Document.Validation.VariablesOfCorrectTypeTest do
 
   use Absinthe.ValidationPhaseCase, async: true, phase: @phase
 
-  defp bad_argument(op_name, var_name, expected_type, inspected_type, line, verbose_errors) do
-    bad_value(
-      Blueprint.Input.Argument,
-      error_message(op_name, var_name, expected_type, inspected_type, verbose_errors),
-      line,
-      name: var_name
-    )
-  end
-
-  defp error_message(op_name, var_name, expected_type, inspected_type, []) do
-    @phase.error_message(op_name, var_name, expected_type, inspected_type)
-  end
-
-  defp error_message(op_name, var_name, expected_type, inspected_type, verbose_errors) do
-    @phase.error_message(op_name, var_name, expected_type, inspected_type) <>
-      " " <> Enum.join(verbose_errors, " ")
-  end
-
   test "types of variables match types of arguments" do
-    assert_fails_validation(
-      """
-      query test($intArg: Int!) {
-        complicatedArgs {
-          stringArgField(stringArg: $intArg)
+    {:ok, %{errors: errors}} =
+      Absinthe.run(
+        """
+        query test($intArg: Int!) {
+          complicatedArgs {
+            stringArgField(stringArg: $intArg)
+          }
         }
-      }
-      """,
-      [variables: %{"intArg" => 5}],
-      [
-        bad_argument("test", "intArg", "Int", "String", 3, [
-          "(from line [%Absinthe.Blueprint.SourceLocation{column: 31, line: 3}])"
-        ])
-      ]
-    )
+        """,
+        Absinthe.Fixtures.PetsSchema,
+        variables: %{"intArg" => 5}
+      )
+
+    expected_error_msg = @phase.error_message("test", "intArg", "Int", "String")
+    assert expected_error_msg in (errors |> Enum.map(& &1.message))
   end
 
   test "types of variables match types of arguments even when the value is null" do
-    assert_fails_validation(
-      """
-      query test($intArg: Int) {
-        complicatedArgs {
-          stringArgField(stringArg: $intArg)
+    {:ok, %{errors: errors}} =
+      Absinthe.run(
+        """
+        query test($intArg: Int) {
+          complicatedArgs {
+            stringArgField(stringArg: $intArg)
+          }
         }
-      }
-      """,
-      [variables: %{"intArg" => nil}],
-      [
-        bad_argument("test", "intArg", "Int", "String", 3, [
-          "(from line [%Absinthe.Blueprint.SourceLocation{column: 31, line: 3}])"
-        ])
-      ]
-    )
+        """,
+        Absinthe.Fixtures.PetsSchema,
+        variables: %{"intArg" => nil}
+      )
+
+    expected_error_msg = @phase.error_message("test", "intArg", "Int", "String")
+    assert expected_error_msg in (errors |> Enum.map(& &1.message))
   end
 
   test "types of variables match types of arguments in named fragments" do
-    assert_fails_validation(
-      """
-      query test($intArg: Int) {
-        complicatedArgs {
-          ...Fragment
+    {:ok, %{errors: errors}} =
+      Absinthe.run(
+        """
+        query test($intArg: Int) {
+          complicatedArgs {
+            ...Fragment
+          }
         }
-      }
 
-      fragment Fragment on ComplicatedArgs {
-        stringArgField(stringArg: $intArg)
-      }
-      """,
-      [variables: %{"intArg" => 5}],
-      [
-        bad_argument("test", "intArg", "Int", "String", 8, [
-          "(from line [%Absinthe.Blueprint.SourceLocation{column: 29, line: 8}])"
-        ])
-      ]
-    )
+        fragment Fragment on ComplicatedArgs {
+          stringArgField(stringArg: $intArg)
+        }
+        """,
+        Absinthe.Fixtures.PetsSchema,
+        variables: %{"intArg" => 5}
+      )
+
+    expected_error_msg = @phase.error_message("test", "intArg", "Int", "String")
+    assert expected_error_msg in (errors |> Enum.map(& &1.message))
   end
 end
