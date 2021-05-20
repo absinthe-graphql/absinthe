56a98bade94ce7c2b45920cfd52c947170ecafdf
Ben Wilson <benwilson512@gmail.com>
2021-05-20 16:08:01 -0400

    example of multiple aliases test

--- test/absinthe/integration/execution/aliases/with_errors_test.exs
+++ test/absinthe/integration/execution/aliases/with_errors_test.exs
@@ -19,4 +19,24 @@ defmodule Elixir.Absinthe.Integration.Execution.Aliases.WithErrorsTest do
               ]
             }} == Absinthe.run(@query, Absinthe.Fixtures.Things.MacroSchema, [])
   end
+
+  @query """
+  mutation {
+    foo: failingThing(type: WITH_CODE) { name }
+    bar: failingThing(type: WITH_CODE) { name }
+  }
+  """
+
+  test "multiple aliases" do
+    assert {
+              :ok,
+              %{
+                data: %{"foo" => nil, "bar" => nil},
+                errors: [
+                  %{code: 42, locations: [%{column: 3, line: 3}], message: "Custom Error", path: ["bar"]},
+                  %{code: 42, locations: [%{column: 3, line: 2}], message: "Custom Error", path: ["foo"]}
+                ]
+              }
+            } == Absinthe.run(@query, Absinthe.Fixtures.Things.MacroSchema, [])
+  end
 end
