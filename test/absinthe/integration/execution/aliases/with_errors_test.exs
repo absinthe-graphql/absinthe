c232f39c5643d2957e3f9b3ab9fe3ac51ed6edc5
Jonatan Männchen <jonatan@maennchen.ch>
2021-06-02 12:39:10 +0100

    Fix Mix Format (#1079)

--- test/absinthe/integration/execution/aliases/with_errors_test.exs
+++ test/absinthe/integration/execution/aliases/with_errors_test.exs
@@ -29,14 +29,24 @@ defmodule Elixir.Absinthe.Integration.Execution.Aliases.WithErrorsTest do
 
   test "multiple aliases" do
     assert {
-              :ok,
-              %{
-                data: %{"foo" => nil, "bar" => nil},
-                errors: [
-                  %{code: 42, locations: [%{column: 3, line: 3}], message: "Custom Error", path: ["bar"]},
-                  %{code: 42, locations: [%{column: 3, line: 2}], message: "Custom Error", path: ["foo"]}
-                ]
-              }
-            } == Absinthe.run(@query, Absinthe.Fixtures.Things.MacroSchema, [])
+             :ok,
+             %{
+               data: %{"foo" => nil, "bar" => nil},
+               errors: [
+                 %{
+                   code: 42,
+                   locations: [%{column: 3, line: 3}],
+                   message: "Custom Error",
+                   path: ["bar"]
+                 },
+                 %{
+                   code: 42,
+                   locations: [%{column: 3, line: 2}],
+                   message: "Custom Error",
+                   path: ["foo"]
+                 }
+               ]
+             }
+           } == Absinthe.run(@query, Absinthe.Fixtures.Things.MacroSchema, [])
   end
 end
