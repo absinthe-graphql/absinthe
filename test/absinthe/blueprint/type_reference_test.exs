1e104eed0847931f6303b4016ba0e371db155dad
Maarten van Vliet <maartenvanvliet@gmail.com>
2022-01-05 16:30:35 +0100

    Add TypeReference.name/1 to get graphql representation
    Necessary to get the name of the type when
    the underlying type may not exist in the schema
    and therefore `Absinthe.Type.name/1` won't work.

--- test/absinthe/blueprint/type_reference_test.exs
+++ test/absinthe/blueprint/type_reference_test.exs
@@ -2,6 +2,7 @@ defmodule Absinthe.Blueprint.TypeReferenceTest do
   use Absinthe.Case, async: true
 
   alias Absinthe.Blueprint
+  @moduletag :f
 
   describe ".unwrap of Name" do
     test "is left intact" do
@@ -37,6 +38,43 @@ defmodule Absinthe.Blueprint.TypeReferenceTest do
       non_null = %Blueprint.TypeReference.NonNull{of_type: name}
       list = %Blueprint.TypeReference.List{of_type: non_null}
       assert Blueprint.TypeReference.unwrap(list) == name
+    end
+  end
+
+  describe "name/1 of Name" do
+    test "returns type name" do
+      name = %Blueprint.TypeReference.Name{name: "Foo"}
+      assert Blueprint.TypeReference.name(name) == "Foo"
+    end
+  end
+
+  describe "name/1 of List" do
+    test "returns type name in list" do
+      name = %Blueprint.TypeReference.Name{name: "Foo"}
+      list = %Blueprint.TypeReference.List{of_type: name}
+      assert Blueprint.TypeReference.name(list) == "[Foo]"
+    end
+
+    test "returns type name, in multiple lists" do
+      name = %Blueprint.TypeReference.Name{name: "Foo"}
+      list_1 = %Blueprint.TypeReference.List{of_type: name}
+      list_2 = %Blueprint.TypeReference.List{of_type: list_1}
+      assert Blueprint.TypeReference.name(list_2) == "[[Foo]]"
+    end
+  end
+
+  describe "name/1 of NonNull" do
+    test "returns non null type name" do
+      name = %Blueprint.TypeReference.Name{name: "Foo"}
+      list = %Blueprint.TypeReference.NonNull{of_type: name}
+      assert Blueprint.TypeReference.name(list) == "Foo!"
+    end
+
+    test "returns nested non null type name" do
+      name = %Blueprint.TypeReference.Name{name: "Foo"}
+      non_null = %Blueprint.TypeReference.NonNull{of_type: name}
+      list = %Blueprint.TypeReference.List{of_type: non_null}
+      assert Blueprint.TypeReference.name(list) == "[Foo!]"
     end
   end
 end
