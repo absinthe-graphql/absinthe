102360eb4dbc289f223cf1cbc64fc01f148721f8
José Valim <jose.valim@dashbit.co>
2022-02-14 15:08:57 +0100

    Return rest in post_traverse to avoid future deprecations (#1151)

--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule Absinthe.Mixfile do
 
   defp deps do
     [
-      {:nimble_parsec, "~> 0.5 or ~> 1.0"},
+      {:nimble_parsec, "~> 1.2.2 or ~> 1.3.0"},
       {:telemetry, "~> 1.0 or ~> 0.4"},
       {:dataloader, "~> 1.0.0", optional: true},
       {:decimal, "~> 1.0 or ~> 2.0", optional: true},
