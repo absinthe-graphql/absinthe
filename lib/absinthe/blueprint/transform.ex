8449ad77e951870c2b408bd830c856351acf081a
Maarten van Vliet <maartenvanvliet@gmail.com>
2022-02-15 21:16:42 +0100

    Visit schema definition type_extensions in bp walk

--- lib/absinthe/blueprint/transform.ex
+++ lib/absinthe/blueprint/transform.ex
@@ -86,7 +86,12 @@ defmodule Absinthe.Blueprint.Transform do
     Blueprint.Schema.InterfaceTypeDefinition => [:interfaces, :fields, :directives],
     Blueprint.Schema.ObjectTypeDefinition => [:interfaces, :fields, :directives],
     Blueprint.Schema.ScalarTypeDefinition => [:directives],
-    Blueprint.Schema.SchemaDefinition => [:directive_definitions, :type_definitions, :directives],
+    Blueprint.Schema.SchemaDefinition => [
+      :directive_definitions,
+      :type_definitions,
+      :type_extensions,
+      :directives
+    ],
     Blueprint.Schema.UnionTypeDefinition => [:directives, :types]
   }
 
