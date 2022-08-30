0771c5f7d28f32a3f802fbe1b7af26eb0a1470ff
Maarten van Vliet <maartenvanvliet@gmail.com>
2022-08-30 14:59:21 +0200

    Raise for non-toplevel root object definitions

--- lib/absinthe/schema.ex
+++ lib/absinthe/schema.ex
@@ -92,6 +92,8 @@ defmodule Absinthe.Schema do
   ```
   """
 
+  Module.register_attribute(__MODULE__, :placement, accumulate: true)
+
   defmacro __using__(opts) do
     Module.register_attribute(__CALLER__.module, :pipeline_modifier,
       accumulate: true,
@@ -151,6 +153,7 @@ defmodule Absinthe.Schema do
   @object_type Absinthe.Blueprint.Schema.ObjectTypeDefinition
 
   @default_query_name "RootQueryType"
+  @placement {:query, [toplevel: true, extend: true]}
   @doc """
   Defines a root Query object
   """
@@ -163,10 +166,13 @@ defmodule Absinthe.Schema do
       raw_attrs
       |> Keyword.put_new(:name, @default_query_name)
 
-    Absinthe.Schema.Notation.record!(env, @object_type, :query, attrs, block)
+    env
+    |> Absinthe.Schema.Notation.recordable!(:query, @placement[:query])
+    |> Absinthe.Schema.Notation.record!(@object_type, :query, attrs, block)
   end
 
   @default_mutation_name "RootMutationType"
+  @placement {:mutation, [toplevel: true, extend: true]}
   @doc """
   Defines a root Mutation object
 
@@ -190,10 +196,13 @@ defmodule Absinthe.Schema do
       raw_attrs
       |> Keyword.put_new(:name, @default_mutation_name)
 
-    Absinthe.Schema.Notation.record!(env, @object_type, :mutation, attrs, block)
+    env
+    |> Absinthe.Schema.Notation.recordable!(:mutation, @placement[:mutation])
+    |> Absinthe.Schema.Notation.record!(@object_type, :mutation, attrs, block)
   end
 
   @default_subscription_name "RootSubscriptionType"
+  @placement {:subscription, [toplevel: true, extend: true]}
   @doc """
   Defines a root Subscription object
 
@@ -291,7 +300,9 @@ defmodule Absinthe.Schema do
       raw_attrs
       |> Keyword.put_new(:name, @default_subscription_name)
 
-    Absinthe.Schema.Notation.record!(env, @object_type, :subscription, attrs, block)
+    env
+    |> Absinthe.Schema.Notation.recordable!(:subscription, @placement[:subscription])
+    |> Absinthe.Schema.Notation.record!(@object_type, :subscription, attrs, block)
   end
 
   defmacro __before_compile__(_) do
