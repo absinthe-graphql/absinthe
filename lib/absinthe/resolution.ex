ee6b536705a61b0ead670cf600547f9ed8b33de2
matthew graham <mattg@path.pub>
2024-09-01 18:19:33 -0400

    fix path_string crash when path includes list
    in Absinthe.Resolution; also:
    
    * add documentation for path_string/1
    * add specs for path/1 and path_string/1
    
    this fix is #1337
    https://github.com/absinthe-graphql/absinthe/issues/1337

--- lib/absinthe/resolution.ex
+++ lib/absinthe/resolution.ex
@@ -128,6 +128,7 @@ defmodule Absinthe.Resolution do
   In this case `5` is the 0 based index in the list of users the field is currently
   at.
   """
+  @spec path(t()) :: [String.t()]
   def path(%{path: path}) do
     path
     |> Enum.reverse()
@@ -235,8 +236,30 @@ defmodule Absinthe.Resolution do
 
   def call(res, _), do: res
 
+  @doc """
+  Get a list of strings representing the path to this field resolution.
+  The index of list items is dropped and the schema type is included.
+
+  ## Examples
+  Given some query:
+  ```graphql
+  query {users { email }}
+  ```
+
+  If you called this function inside a resolver on the users email field it
+  returns a value like:
+
+  ```elixir
+  resolve fn _, _, resolution ->
+    Absinthe.Resolution.path(resolution) #=> ["email", "users", "RootQueryType"]
+  end
+  ```
+  """
+  @spec path_string(t()) :: [String.t()]
   def path_string(%__MODULE__{path: path}) do
-    Enum.map(path, fn
+    path
+    |> Enum.reject(fn x -> is_integer(x) end)
+    |> Enum.map(fn
       %{name: name, alias: alias} ->
         alias || name
 
