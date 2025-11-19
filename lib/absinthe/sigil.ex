a1c98e8e36c89f77f2a4dde66c859221d2f99206
Greg Vaughn <gvaughn@gmail.com>
2025-11-19 09:55:15 -0600

    add GQL sigil to format and lint static GraphQL docs particularly useful in tests

---
+++ lib/absinthe/sigil.ex
@@ -0,0 +1,31 @@
+defmodule Absinthe.Sigil do
+  @moduledoc """
+  Provides the ~GQL sigil
+  """
+
+  def sigil_GQL(string, []) do
+    case Absinthe.Phase.Parse.run(string, []) do
+      {:ok, blueprint} ->
+        inspect(blueprint.input, pretty: true)
+
+      {:error, %Absinthe.Blueprint{execution: %{validation_errors: [_ | _] = errors}}} ->
+        {:current_stacktrace, [_process_info, _absinthe_sigil, {_, _, _, loc} | _]} =
+          Process.info(self(), :current_stacktrace)
+
+        err_string =
+          Enum.join(
+            [
+              "~GQL sigil validation error at " <> inspect(loc)
+              | Enum.map(errors, &"#{&1.message} (#{inspect(&1.locations)})")
+            ],
+            "\n"
+          )
+
+        IO.puts(:stderr, err_string)
+        string
+
+      other ->
+        other
+    end
+  end
+end
